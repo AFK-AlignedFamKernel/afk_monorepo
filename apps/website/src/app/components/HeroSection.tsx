d172162ebfa5aa029ac7ab2c104cb589485c8239
MSghais <msghais135@gmail.com>
2024-08-04 15:35:13 +0200

    lint fix

--- apps/website/src/app/components/HeroSection.tsx
+++ apps/website/src/app/components/HeroSection.tsx
@@ -1,28 +1,28 @@
 'use client';
 
-import { motion } from 'framer-motion';
+import {motion} from 'framer-motion';
 
 export function HeroSection() {
   return (
     <div className="desktop:h-[900px] h-[833px] w-full overflow-hidden relative pt-[98px] desktop:pt-[159px] flex justify-center bg-black desktop:bg-herobg bg-mobileHeroBg bg-no-repeat bg-bottom">
       <motion.img
-        animate={{ x: [500, 0] }}
+        animate={{x: [500, 0]}}
         transition={{
-          x: { duration: 1 },
+          x: {duration: 1},
         }}
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
         src="/assets/moon.svg"
         className="absolute top-5 desktop:top-[36px] right-[31px] desktop:right-[150px] w-[50px] desktop:w-auto"
         alt=""
       />
       <motion.div
-        animate={{ y: [1200, 0] }}
+        animate={{y: [1200, 0]}}
         transition={{
-          y: { duration: 1 },
+          y: {duration: 1},
         }}
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
         className="desktop:w-[773px] w-[85%] flex flex-col items-center text-white h-fit text-center relative z-[270]"
       >
         <h1 className="desktop:text-[72px] text-[36px] leading-[50px] desktop:leading-[80px] mb-3">
@@ -48,12 +48,12 @@ export function HeroSection() {
         src="/assets/degen-logo.png"
         className="absolute left-[-31px] desktop:left-2 bottom-[39px] desktop:bottom-[49px] z-[250] desktop:w-[380px] w-[210px]"
         alt=""
-        animate={{ x: [-500, 0] }}
+        animate={{x: [-500, 0]}}
         transition={{
-          x: { duration: 1 },
+          x: {duration: 1},
         }}
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
       />
     </div>
   );
