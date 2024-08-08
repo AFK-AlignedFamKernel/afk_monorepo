1ab10e94a9f5ffbaf2a1cf1ef6bb3fda64cb2b4a
MSGhais <msghais135@gmail.com>
2024-08-08 19:04:14 +0200

    lint fix

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,13 +1,14 @@
 import './index.css';
 
-import type { Metadata } from 'next';
+import type {Metadata} from 'next';
+
 import Providers from './providers';
 export const metadata: Metadata = {
   title: 'afk community portal',
   description: 'afk community portal',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <Providers>
