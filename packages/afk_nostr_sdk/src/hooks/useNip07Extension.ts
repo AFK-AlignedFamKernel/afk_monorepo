3b9923f2b3c7937f2fdc3cebbaf53d5873ce60d5
MSGhais <msghais135@gmail.com>
2024-08-21 00:59:26 +0200

    clean

--- packages/afk_nostr_sdk/src/hooks/useNip07Extension.ts
+++ packages/afk_nostr_sdk/src/hooks/useNip07Extension.ts
@@ -1,6 +1,5 @@
 import {useNostrContext} from '../context/NostrContext';
 import {useAuth} from '../store';
-
 export type UseRootProfilesOptions = {
   authors?: string[];
   search?: string;
@@ -9,12 +8,10 @@ export type UseRootProfilesOptions = {
 export const useNip07Extension = (options?: UseRootProfilesOptions) => {
   const {ndk, nip07Signer} = useNostrContext();
   const {setAuth, setPublicKey, setIsExtensionConnect} = useAuth();
-
   const getPublicKey = async () => {
     if (typeof window != 'undefined') {
       const pubkey = await window.nostr.getPublicKey();
       const created_at = new Date().getTime();
-
       nip07Signer?.user().then(async (user) => {
         if (user.npub) {
           console.log('Permission granted to read their public key:', user.npub);
@@ -28,7 +25,6 @@ export const useNip07Extension = (options?: UseRootProfilesOptions) => {
       // }
     }
   };
-
   return {
     getPublicKey,
   };
