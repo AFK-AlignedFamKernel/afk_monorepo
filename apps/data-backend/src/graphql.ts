4621a74668ebc97df6e47c7a951444f8c09fffbd
Fishon Amos <amosfishon@gmail.com>
2024-08-23 23:14:01 +0100

    only get events

--- apps/data-backend/src/graphql.ts
+++ apps/data-backend/src/graphql.ts
@@ -42,40 +42,9 @@ const typeDefs = gql`
     tokenDeploys: [TokenDeploy]
     tokenDeploy(memecoinAddress: String!): TokenDeploy
   }
-  type Mutation {
-    createBuyToken(
-      network: String!
-      blockHash: String!
-      blockNumber: Int!
-      blockTimestamp: String!
-      transactionHash: String!
-      memecoinAddress: String!
-      ownerAddress: String!
-      price: String!
-      amount: String!
-      protocolFee: String!
-      initialSupply: String!
-      timestamp: String!
-    ): BuyToken
-
-    updateBuyToken(
-      transactionHash: String!
-      data: UpdateBuyTokenInput!
-    ): BuyToken
-
-    deleteBuyToken(transactionHash: String!): BuyToken
-  }
-
-  input UpdateBuyTokenInput {
-    price: String
-    amount: String
-    protocolFee: String
-    initialSupply: String
-    timestamp: String
-  }
 `;
 
-// Define resolvers for the schema
+// Resolvers for the schema
 const resolvers = {
   Query: {
     buyTokens: () => prisma.buy_token.findMany(),
@@ -87,24 +56,11 @@ const resolvers = {
       where: { memecoin_address: memecoinAddress },
     }),
   },
-  Mutation: {
-    createBuyToken: (_parent, args) => prisma.buy_token.create({
-      data: args,
-    }),
-    updateBuyToken: (_parent, { transactionHash, data }) => prisma.buy_token.update({
-      where: { transactionHash },
-      data,
-    }),
-    deleteBuyToken: (_parent, { transactionHash }) => prisma.buy_token.delete({
-      where: { transactionHash },
-    }),
-  },
 };
-
 
 // Create the Apollo Server
 const server = new ApolloServer({ 
   typeDefs,
   resolvers,
   context: () => ({ prisma }),
-  });
+});
