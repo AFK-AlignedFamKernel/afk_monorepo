4bd43e00c499c364ebd32c40a3e3ce7714bdfcab
Nnaji Benjamin <60315147+Benjtalkshow@users.noreply.github.com>
2024-08-25 14:28:23 +0100

    Nostr repost hook (#56)
    * Feat: Implement repost hook (useRepost)
    
    * Chore: Implement repost hook
    
    * fix: useRepost

--- apps/mobile/src/modules/PostCard/index.tsx
+++ apps/mobile/src/modules/PostCard/index.tsx
@@ -1,17 +1,15 @@
-import {NDKEvent} from '@nostr-dev-kit/ndk';
-import {View} from 'react-native';
-
-import {useStyles} from '../../hooks';
-import {Post} from '../Post';
+import { NDKEvent } from '@nostr-dev-kit/ndk';
+import { View } from 'react-native';
+import { useStyles } from '../../hooks';
+import { Post } from '../Post';
 import stylesheet from './styles';
 
 export type PostCardProps = {
   event?: NDKEvent;
 };
 
-export const PostCard: React.FC<PostCardProps> = ({event}) => {
+export const PostCard: React.FC<PostCardProps> = ({ event }) => {
   const styles = useStyles(stylesheet);
-
   return (
     <View style={styles.container}>
       <Post event={event} />
