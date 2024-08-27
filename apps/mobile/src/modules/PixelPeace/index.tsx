cdecb20689255e6ba3884bb763a498528fe125e6
MSGhais <msghais135@gmail.com>
2024-08-27 17:00:41 +0200

    setup +fix hook media query + webview fix

--- apps/mobile/src/modules/PixelPeace/index.tsx
+++ apps/mobile/src/modules/PixelPeace/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useStyles } from '../../hooks';
 import stylesheet from './styles';
 import { Chat } from '../../components/PrivateMessages/Chat';
-import { App, AppRender } from "pixel_ui"
+// import { App, AppRender } from "pixel_ui"
 import WebView from 'react-native-webview';
 import { Platform, View } from 'react-native';
 import { useIsDesktop } from '../../hooks/useIsDesktop';
@@ -18,27 +18,22 @@ export const PixelPeace: React.FC = () => {
       style={{ height: "100%" }}
     >
 
-      {Platform.OS == "web"
-
-        &&
-
+      {Platform.OS == "web" && process.env.EXPO_PUBLIC_PIXEL_URL &&
         <>
           <iframe src={process.env.EXPO_PUBLIC_PIXEL_URL}
             height={isDesktop ? 750 : 550}
-            // height={750}
             width={"100%"}
           ></iframe>
 
         </>
       }
 
-      {Platform.OS != "web" &&
+      {Platform.OS != "web" && process.env.EXPO_PUBLIC_PIXEL_URL &&
         <WebView
+          source={{ uri: process.env.EXPO_PUBLIC_PIXEL_URL }}
         >
-          <AppRender></AppRender>
         </WebView>
       }
-
     </View>
 
   );
