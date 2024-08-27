bce7d3a314051dee5fee17f71a642d287d87ed32
MSGhais <msghais135@gmail.com>
2024-08-27 14:33:32 +0200

    typo

--- packages/pixel_ui/src/canvas/TemplateCreationOverlay.js
+++ packages/pixel_ui/src/canvas/TemplateCreationOverlay.js
@@ -98,7 +98,7 @@ const TemplateCreationOverlay = (props) => {
     const imageData = ctx.getImageData(0, 0, image.width, image.height);
     const data = imageData.data;
 
-    let imagePalleteIds = [];
+    let imagePaletteIds = [];
     // Convert image data to color palette
     for (let i = 0; i < data.length; i += 4) {
       if (data[i + 3] < 128) {
@@ -106,7 +106,7 @@ const TemplateCreationOverlay = (props) => {
         data[i + 1] = 255;
         data[i + 2] = 255;
         data[i + 3] = 0;
-        imagePalleteIds.push(255);
+        imagePaletteIds.push(255);
         continue;
       }
       let minDistance = 1000000;
@@ -130,7 +130,7 @@ const TemplateCreationOverlay = (props) => {
       data[i] = minColor[0];
       data[i + 1] = minColor[1];
       data[i + 2] = minColor[2];
-      imagePalleteIds.push(minColorIndex);
+      imagePaletteIds.push(minColorIndex);
     }
 
     // Set image data back to canvas
