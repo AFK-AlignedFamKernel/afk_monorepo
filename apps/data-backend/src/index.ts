520bf582cfb0cd186f610e60e09cef0d6f8947c7
Fishon Amos <amosfishon@gmail.com>
2024-08-27 14:35:08 +0100

    remove comments

--- apps/data-backend/src/index.ts
+++ apps/data-backend/src/index.ts
@@ -1,39 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import router from "./router";
-// import helmet from "helmet";
-// import { launchBot, sendWebAppButton } from "./services/telegram-app";
-// const cors = require("cors");
-
-// dotenv.config();
-// const app = express();
-
-// app.use(helmet());
-// app.use(cors({
-//     origin: 'http://localhost:8081' // Adjust this to your frontend application URL
-// }));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-
-// app.use('/', router);
-
-// const port = process.env.PORT || 5050;
-// app.listen(port, () => {
-//     console.log(`🚀 Backend server running at http://localhost:${port}`);
-//     try {
-//         launchBot(process.env.TELEGRAM_BOT_TOKEN);
-//     } catch (error) {
-//         console.error("Error launching bot:", error);
-//     }
-// });
-
-// // Optionally re-enable GraphQL if you plan to use it
-// /*
-// server.listen({ port: 4000 }).then(({ url }) => {
-//     console.log(`🚀 GraphQL server ready at ${url}`);
-// });
-// */
-
 import express from "express";
 import dotenv from "dotenv";
 import router from "./router";
