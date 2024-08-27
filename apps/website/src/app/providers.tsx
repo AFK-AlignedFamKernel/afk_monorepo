16851fa9261b1a668ea1933bacb5e73160ccd721
MSGhais <msghais135@gmail.com>
2024-08-27 17:31:26 +0200

    no import package nostr => go fix

--- apps/website/src/app/providers.tsx
+++ apps/website/src/app/providers.tsx
@@ -4,7 +4,7 @@ import StarknetProvider from '@/context/StarknetProvider';
 
 // import {TanstackProvider} from 'afk_nostr_sdk';
 // import {NostrProvider} from 'afk_nostr_sdk';
-export default function Providers({children}: {children: React.ReactNode}) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
       <StarknetProvider>
