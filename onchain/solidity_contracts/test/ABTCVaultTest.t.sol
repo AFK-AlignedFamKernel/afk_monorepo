9ca4e7c824a63440cba64313b0141ce99667827e
Poulav Bhowmick <bpoulav@gmail.com>
2024-08-29 01:12:56 +0530

    added ABTCVault test file2

--- onchain/solidity_contracts/test/ABTCVaultTest.t.sol
+++ onchain/solidity_contracts/test/ABTCVaultTest.t.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.0;
 
 import "forge-std/Test.sol";
-import "../src/defi/Vault.sol";
+import "../src/defi/ABTCVault.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockWrappedBTC is ERC20 {
