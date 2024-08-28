edd49acdd27685bea57330b09e7bd1504ba1df6a
Fishon Amos <amosfishon@gmail.com>
2024-08-28 21:40:48 +0100

    event.data

--- apps/indexer/src/deploy-token.js
+++ apps/indexer/src/deploy-token.js
@@ -1,70 +1,76 @@
-import { LAUNCHPAD_ADDRESS, STARTING_BLOCK } from './constants.js'
 import { hash, uint256, shortString } from "https://esm.run/starknet@5.14";
+import { STARTING_BLOCK, LAUNCHPAD_ADDRESS } from "./constants";
 
 const filter = {
-  header: {
-    weak: true,
-  },
-  events: [
-    {
-      fromAddress: LAUNCHPAD_ADDRESS.SEPOLIA,
-      keys: [hash.getSelectorFromName('CreateToken')],
-      includeReceipt: false,
+    header: {
+        weak: true,
     },
-  ],
+    events: [
+        {
+            fromAddress: LAUNCHPAD_ADDRESS.SEPOLIA,
+            keys: [hash.getSelectorFromName('CreateLaunch')],
+            includeReceipt: false,
+        },
+    ],
 }
 
 export const config = {
-  streamUrl: 'https://sepolia.starknet.a5a.ch',
-  startingBlock: STARTING_BLOCK,
-  network: 'starknet',
-  finality: 'DATA_STATUS_ACCEPTED',
-  filter,
-  sinkType: 'postgres',
-  sinkOptions: {
-    connectionString: '',
-    tableName: 'token_deploy',
-  },
+    streamUrl: 'https://sepolia.starknet.a5a.ch',
+    startingBlock: STARTING_BLOCK,
+    network: 'starknet',
+    finality: 'DATA_STATUS_ACCEPTED',
+    filter,
+    sinkType: 'postgres',
+    sinkOptions: {
+        connectionString: 'Your Connection String Here',
+        tableName: 'token_launch',
+    },
 }
 
-export default function DecodeTokenDeploy({ header, events }) {
-  const { blockNumber, blockHash, timestamp } = header;
+export default function DecodeTokenLaunchDeploy({ header, events }) {
+    const { blockNumber, blockHash, timestamp } = header;
 
-  return (events ?? []).map(({ event, transaction }) => {
-    if (!event.data || !event.keys) return;
+    return (events ?? []).map(({ event, transaction }) => {
+        if (!event.data) return;
 
-    const transactionHash = transaction.meta.hash;
-    const [caller, token_address,
-      // name, symbol
-    ] = event.keys;
+        const transactionHash = transaction.meta.hash;
+        const [
+            owner, 
+            token_address,
+            name,
+            symbol,
+            initial_supply_low, 
+            initial_supply_high, 
+            total_supply_low, 
+            total_supply_high,
+            quote_token,
+            exchange_name,
+            price
+        ] = event.data;
 
-    const [
-      initial_supply_low, initial_supply_high,
-      total_supply_low, total_supply_high,
-      symbol, name
-    ] = event.data;
+        const initial_supply = uint256.uint256ToBN({ low: initial_supply_low, high: initial_supply_high }).toString();
+        const total_supply = uint256.uint256ToBN({ low: total_supply_low, high: total_supply_high }).toString();
+        const name_decoded = shortString.decodeShortString(name);
+        const symbol_decoded = shortString.decodeShortString(symbol);
+        const price_decoded = price ? formatUnits(price, 18) : '0';
 
-    const initial_supply = uint256.uint256ToBN({ low: initial_supply_low, high: initial_supply_high }).toString();
-    const total_supply = uint256.uint256ToBN({ low: total_supply_low, high: total_supply_high }).toString();
-    const name_decoded = shortString.decodeShortString(name);
-    const symbol_decoded = shortString.decodeShortString(symbol);
-
-    return {
-      network: 'starknet-sepolia',
-      block_hash: blockHash,
-      block_number: Number(blockNumber),
-      block_timestamp: timestamp,
-      transaction_hash: transactionHash,
-      memecoin_address: token_address,
-      owner_address: caller,
-      name: name_decoded,
-      symbol: symbol_decoded,
-      initial_supply,
-      total_supply,
-      created_at: new Date().toISOString(),
-      _cursor: transaction.meta.cursor,
-      timestamp: new Date(timestamp * 1000).toISOString(),
-
-    };
-  });
+        return {
+            network: 'starknet-sepolia',
+            block_hash: blockHash,
+            block_number: Number(blockNumber),
+            block_timestamp: new Date(timestamp * 1000).toISOString(),
+            transaction_hash: transactionHash,
+            memecoin_address: token_address,
+            owner_address: owner,
+            name: name_decoded,
+            symbol: symbol_decoded,
+            initial_supply,
+            total_supply,
+            quote_token: quote_token ? shortString.decodeShortString(quote_token) : '',
+            exchange_name: exchange_name ? shortString.decodeShortString(exchange_name) : '',
+            price: price_decoded,
+            created_at: new Date().toISOString(),
+            cursor: transaction.meta.cursor,
+        };
+    });
 }
