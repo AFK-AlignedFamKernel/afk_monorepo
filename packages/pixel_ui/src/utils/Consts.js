3c75b2d534404981d1176d1202b81b12589ef8fa
MSG <59928086+MSghais@users.noreply.github.com>
2024-08-29 14:35:58 +0200

    pump state and transfer + fix pixel backend (#94)

--- packages/pixel_ui/src/utils/Consts.js
+++ packages/pixel_ui/src/utils/Consts.js
@@ -1,27 +1,52 @@
 import backendConfig from '../configs/backend.config.json';
 
+/** TODO add ENV and config for prod and test */
 /** TODO fix url */
 // TODO used REACT_APP_NODE_ENV
-
+// const isProduction = process.env.REACT_APP_NODE_ENV == "true" ? true : false
+const isProduction = true
 // export const backendUrl = 'https://' + backendConfig.host;
-export const backendUrl =  backendConfig.host;
-// export const backendUrl = 'https://' + backendConfig.host + ':' + backendConfig.port;
+console.log("isProduction", isProduction)
+console.log("REACT_APP_BACKEND_URL", process.env.REACT_APP_BACKEND_URL)
+// export const backendUrl = process.env.REACT_APP_BACKEND_URL ?? backendConfig.host;
 
-// export const backendUrl = backendConfig.production
-//   ? 'https://' + backendConfig.host
-//   : 'http://' + backendConfig.host + ':' + backendConfig.port;
+// export const backendUrl = process.env.REACT_APP_BACKEND_URL ? 'https://' + process.env.REACT_APP_BACKEND_URL : 'https://' + backendConfig.host + ':' + backendConfig.port;
+// console.log("backendUrl", backendUrl)
 
-export const wsUrl = backendConfig.production
+export const backendUrl = isProduction
+  ? 'https://' + typeof process.env.REACT_APP_BACKEND_URL !== "undefined" ? process.env.REACT_APP_BACKEND_URL : backendConfig.host
+  : 'http://' + backendConfig.host + ':' + backendConfig.port;
+console.log("backendUrl", backendUrl)
+
+export const wsUrl = isProduction
   ? 'wss://' + backendConfig.host + '/ws'
   : 'ws://' + backendConfig.host + ':' + backendConfig.consumer_port + '/ws';
+console.log("wsUrl", wsUrl)
 
-export const nftUrl = backendConfig.production
-  ? 'https://' + backendConfig.host
+export const nftUrl = isProduction
+  ? 'https://' + typeof process.env.REACT_APP_BACKEND_URL !== "undefined" ? process.env.REACT_APP_BACKEND_URL : backendConfig.host
   : 'http://' + backendConfig.host + ':' + backendConfig.consumer_port;
 
-export const templateUrl = backendConfig.production
-  ? 'https://' + backendConfig.host
-  : 'http://' + backendConfig.host + ':' + backendConfig.port;
+
+export const templateUrl = isProduction
+  ? 'https://' + typeof process.env.REACT_APP_BACKEND_URL !== "undefined" ? process.env.REACT_APP_BACKEND_URL : backendConfig.host
+  : 'http://' + typeof process.env.REACT_APP_BACKEND_URL !== "undefined" ? process.env.REACT_APP_BACKEND_URL : backendConfig.host + ':' + backendConfig.port;
+
+
+// export const wsUrl = backendConfig.production
+//   ? 'wss://' + backendConfig.host + '/ws'
+//   : 'ws://' + backendConfig.host + ':' + backendConfig.consumer_port + '/ws';
+// console.log("wsUrl", wsUrl)
+
+// export const nftUrl = backendConfig.production
+//   ? 'https://' + typeof process.env.REACT_APP_BACKEND_URL !== "undefined" ? process.env.REACT_APP_BACKEND_URL : backendConfig.host
+//   : 'http://' + backendConfig.host + ':' + backendConfig.consumer_port;
+
+console.log("nftUrl", nftUrl)
+
+// export const templateUrl = backendConfig.production
+//   ? 'https://' + typeof process.env.REACT_APP_BACKEND_URL !== "undefined" ? process.env.REACT_APP_BACKEND_URL : backendConfig.host
+//   : 'http://' + typeof process.env.REACT_APP_BACKEND_URL !== "undefined" ? process.env.REACT_APP_BACKEND_URL : backendConfig.host + ':' + backendConfig.port;
 
 // TODO used REACT_APP_NODE_ENV
 export const devnetMode = backendConfig.production === false;
