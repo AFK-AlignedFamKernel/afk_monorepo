e495e93ff472d51a55b73c46335db4e3372a31a4
MSGhais <msghais135@gmail.com>
2024-08-30 14:27:34 +0200

    start check admin group

--- apps/mobile/src/modules/Group/message/GroupMessage.tsx
+++ apps/mobile/src/modules/Group/message/GroupMessage.tsx
@@ -1,40 +1,74 @@
-import {useQueryClient} from '@tanstack/react-query';
+import { useQueryClient } from '@tanstack/react-query';
 import {
+  AdminGroupPermission,
   useAuth,
   useGetGroupMemberList,
+  useGetGroupMemberListPubkey,
   useGetGroupMessages,
+  useJoinGroupRequest,
+  useNostrContext,
   useProfile,
   useSendGroupMessages,
 } from 'afk_nostr_sdk';
-import React, {useState} from 'react';
-import {FlatList, Modal, Pressable, SafeAreaView, Text, TouchableOpacity, View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList, Modal, Pressable, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 
-import {BackIcon, MenuIcons} from '../../../assets/icons';
-import {IconButton, Input, KeyboardFixedView} from '../../../components';
-import {useStyles} from '../../../hooks';
-import {useToast} from '../../../hooks/modals';
-import {GroupChatScreenProps} from '../../../types';
+import { BackIcon, MenuIcons } from '../../../assets/icons';
+import { IconButton, Input, KeyboardFixedView } from '../../../components';
+import { useStyles } from '../../../hooks';
+import { useToast } from '../../../hooks/modals';
+import { GroupChatScreenProps } from '../../../types';
 import stylesheet from './styles';
+import { checkGroupPermission } from 'afk_nostr_sdk/src/hooks/group/private/useGetPermission';
+import { NDKEvent } from '@nostr-dev-kit/ndk';
 
-const GroupChat: React.FC<GroupChatScreenProps> = ({navigation, route}) => {
-  const {publicKey} = useAuth();
-  const memberListData = useGetGroupMemberList({
+const GroupChat: React.FC<GroupChatScreenProps> = ({ navigation, route }) => {
+  const { publicKey } = useAuth();
+  const ndk = useNostrContext()
+  const groupdId = route.params.groupId
+
+  const memberListData = useGetGroupMemberListPubkey({
     groupId: route.params.groupId,
   });
-  const profile = useProfile({publicKey});
+
+  const joinRequest = useJoinGroupRequest()
+  const profile = useProfile({ publicKey });
   const [menuVisible, setMenuVisible] = useState(false);
+  const [isUserMember, setIsUserMember] = useState(false);
   const [replyToId, setReplyToId] = useState(null);
   const [replyToContent, setReplyToContent] = useState('');
   const [selectedMessageId, setSelectedMessageId] = useState(null);
   const queryClient = useQueryClient();
-  const {showToast} = useToast();
-  const {data: messageData} = useGetGroupMessages({
+  const { showToast } = useToast();
+  const { data: messageData } = useGetGroupMessages({
     groupId: route.params.groupId,
     authors: publicKey,
   });
-  const {mutate} = useSendGroupMessages();
+  const { mutate } = useSendGroupMessages();
   const styles = useStyles(stylesheet);
   const [message, setMessage] = useState('');
+
+  /** Todo check if user is a member */
+  useEffect(() => {
+
+    const getUserIsMember = () => {
+      let isMember = memberListData?.data?.pages?.flat().find((e: NDKEvent) => {
+        console.log("e", e)
+        const pubkey = e?.tags?.find((tag: string[]) => tag[0] === 'p')?.[1];
+        console.log("pubKey", pubkey)
+        if (pubkey == publicKey) {
+          console.log("pubkey == publicKey", pubkey == publicKey)
+
+          return e
+        }
+        return undefined;
+      })
+      if (isMember) setIsUserMember(true)
+    }
+
+    getUserIsMember()
+
+  }, [memberListData])
 
   const handleLongPress = (messageId: any, messageContent: string) => {
     setSelectedMessageId(messageId);
@@ -62,8 +96,8 @@ const GroupChat: React.FC<GroupChatScreenProps> = ({navigation, route}) => {
       },
       {
         onSuccess() {
-          showToast({type: 'success', title: 'Message sent successfully'});
-          queryClient.invalidateQueries({queryKey: ['getGroupMessages', route.params.groupId]});
+          showToast({ type: 'success', title: 'Message sent successfully' });
+          queryClient.invalidateQueries({ queryKey: ['getGroupMessages', route.params.groupId] });
           setMessage('');
           setReplyToId(null);
           setReplyToContent('');
@@ -76,6 +110,23 @@ const GroupChat: React.FC<GroupChatScreenProps> = ({navigation, route}) => {
         },
       },
     );
+  };
+
+  const requestToJoin = () => {
+
+    joinRequest.mutateAsync({
+      groupId: groupdId,
+
+
+
+    },
+      {
+        onSuccess: () => {
+
+        },
+      },
+    )
+
   };
 
   return (
@@ -103,17 +154,33 @@ const GroupChat: React.FC<GroupChatScreenProps> = ({navigation, route}) => {
           <MenuIcons stroke="gray" />
         </TouchableOpacity>
       </View>
-      <FlatList
-        data={messageData.pages.flat()}
-        renderItem={({item}: any) => <MessageCard handleLongPress={handleLongPress} item={item} />}
-        keyExtractor={(item: any) => item.id}
-        contentContainerStyle={styles.messageList}
-        inverted
-      />
+
+      {isUserMember &&
+        <FlatList
+          data={messageData.pages.flat()}
+          renderItem={({ item }: any) => <MessageCard handleLongPress={handleLongPress} item={item} />}
+          keyExtractor={(item: any) => item.id}
+          contentContainerStyle={styles.messageList}
+          inverted
+        />
+      }
+
+      {!isUserMember &&
+
+        <View>
+
+          <Text>You are not a member</Text>
+          <Text>Request to join the group</Text>
+
+          <IconButton onPress={() => requestToJoin()} icon="SendIcon" size={24} />
+
+        </View>
+      }
+
 
       {replyToId && <ReplyIndicator message={replyToContent} onCancel={cancelReply} />}
 
-      <KeyboardFixedView containerProps={{style: styles.inputContainer}}>
+      <KeyboardFixedView containerProps={{ style: styles.inputContainer }}>
         <View style={styles.inputContent}>
           <Input
             multiline
@@ -171,7 +238,7 @@ const MessageCard = ({
   );
 };
 
-const ReplyIndicator = ({message, onCancel}: {onCancel: () => void; message: string}) => {
+const ReplyIndicator = ({ message, onCancel }: { onCancel: () => void; message: string }) => {
   const styles = useStyles(stylesheet);
   return (
     <View style={styles.replyIndicator}>
@@ -202,7 +269,7 @@ const LongPressMenu = ({
       <Pressable style={styles.modalOverlay} onPress={onClose}>
         <View style={styles.menuContainer}>
           <Pressable style={styles.menuItem} onPress={onReply}>
-            <Text style={{color: 'white'}}>Reply Message</Text>
+            <Text style={{ color: 'white' }}>Reply Message</Text>
           </Pressable>
         </View>
       </Pressable>
