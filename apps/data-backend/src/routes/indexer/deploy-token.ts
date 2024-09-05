49e83e1fe1af3f83e8317247e076decf1578bedc
EjembiEmmanuel <ejembioche50@gmail.com>
2024-09-05 13:04:00 +0100

    fix: fix deploy-token endpoint

--- apps/data-backend/src/routes/indexer/deploy-token.ts
+++ apps/data-backend/src/routes/indexer/deploy-token.ts
@@ -7,44 +7,18 @@ const Router = express.Router();
 
 Router.get("/", async (req, res) => {
   try {
-    // const deploys = await prisma.token_deploy.findMany({});
-
-    // const data = deploys.map((item) => ({
-    //   ...item,
-    //   block_number: item.block_number ? Number(item.block_number) : null,
-    //   cursor: item.cursor ? Number(item.cursor) : null
-    // }));
-
-    // res.status(HTTPStatus.OK).json({
-    //   data: data
-    // });
-
-    const { tokenAddress } = req.params;
-    if (!isValidStarknetAddress(tokenAddress)) {
-      res
-        .status(HTTPStatus.BadRequest)
-        .send({ message: "Invalid token address" });
-      return;
-    }
-
-    const distributions = await prisma.token_transactions.groupBy({
-      by: ["owner_address"],
-      where: { memecoin_address: tokenAddress },
-      _sum: {
-        amount: true
-      },
-      _count: {
-        owner_address: true
+    const deploys = await prisma.token_deploy.findMany({
+      select: {
+        memecoin_address: true,
+        name: true,
+        total_supply: true,
+        network: true
       }
     });
 
-    if (distributions.length === 0) {
-      return res.status(HTTPStatus.NotFound).json({
-        error: "No holders found for this token address."
-      });
-    }
-
-    res.status(HTTPStatus.OK).json(distributions);
+    res.status(HTTPStatus.OK).json({
+      data: deploys
+    });
   } catch (error) {
     console.error("Failed to fetch token deploys:", error);
     res.status(HTTPStatus.InternalServerError).send("Internal Server Error");
