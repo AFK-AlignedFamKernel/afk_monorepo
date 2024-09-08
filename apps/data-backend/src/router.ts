540fe256265543e8d9b0f979c83f51d6d8a5f34a
EjembiEmmanuel <ejembioche50@gmail.com>
2024-09-08 16:01:31 +0100

    feat: migrate `graph` route to fastify

--- apps/data-backend/src/router.ts
+++ apps/data-backend/src/router.ts
@@ -2,11 +2,13 @@ import type { FastifyInstance } from "fastify";
 import buyCoinRoute from "./routes/indexer/buy-coin";
 import deployLaunchRoute from "./routes/indexer/deploy-launch";
 import deployTokenRoute from "./routes/indexer/deploy-token";
+import graphRoute from "./routes/indexer/graph";
 
 function declareRoutes(fastify: FastifyInstance) {
   fastify.register(buyCoinRoute);
   fastify.register(deployLaunchRoute);
   fastify.register(deployTokenRoute);
+  fastify.register(graphRoute);
 }
 
 export default declareRoutes;
