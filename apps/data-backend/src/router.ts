120bdce241e91b1f0700511142e2104a5c943198
EjembiEmmanuel <ejembioche50@gmail.com>
2024-09-08 15:43:37 +0100

    feat: migrate `deploy-token` route to fastify

--- apps/data-backend/src/router.ts
+++ apps/data-backend/src/router.ts
@@ -1,10 +1,12 @@
 import type { FastifyInstance } from "fastify";
 import buyCoinRoute from "./routes/indexer/buy-coin";
 import deployLaunchRoute from "./routes/indexer/deploy-launch";
+import deployTokenRoute from "./routes/indexer/deploy-token";
 
 function declareRoutes(fastify: FastifyInstance) {
   fastify.register(buyCoinRoute);
   fastify.register(deployLaunchRoute);
+  fastify.register(deployTokenRoute);
 }
 
 export default declareRoutes;
