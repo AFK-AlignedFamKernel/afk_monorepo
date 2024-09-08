ec301f8217bdb8791939dba66a89aa5fda1fbe36
EjembiEmmanuel <ejembioche50@gmail.com>
2024-09-08 15:28:38 +0100

    feat: migrate `deploy-launch` endpoint to fastify

--- apps/data-backend/src/router.ts
+++ apps/data-backend/src/router.ts
@@ -1,8 +1,10 @@
 import type { FastifyInstance } from "fastify";
 import buyCoinRoute from "./routes/indexer/buy-coin";
+import deployLaunchRoute from "./routes/indexer/deploy-launch";
 
 function declareRoutes(fastify: FastifyInstance) {
   fastify.register(buyCoinRoute);
+  fastify.register(deployLaunchRoute);
 }
 
 export default declareRoutes;
