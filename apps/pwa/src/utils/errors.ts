4ea06c76a3869951319f2ffd54317eacf3717e6f
Lindsay Morales <lindsaymorales15@gmail.com>
2024-10-01 20:35:59 -0600

    fix errors

--- apps/pwa/src/utils/errors.ts
+++ apps/pwa/src/utils/errors.ts
@@ -1,18 +1,24 @@
-const ErrorCodesArray = [
-  'BAD_REQUEST',
-  'INVALID_EVENT',
-  'INVALID_EVENT_SIGNATURE',
-  'INVALID_EVENT_CONTENT',
-  'INVALID_ADDRESS',
-  'DEPOSIT_NOT_FOUND',
-  'INVALID_GAS_AMOUNT',
-  'TRANSACTION_ERROR',
-  'ESTIMATION_ERROR',
-  'NO_ROUTE_FOUND',
-] as const;
+export const ErrorCodes = {
+  BAD_REQUEST: 'BAD_REQUEST',
+  INVALID_EVENT: 'INVALID_EVENT',
+  INVALID_EVENT_SIGNATURE: 'INVALID_EVENT_SIGNATURE',
+  INVALID_EVENT_CONTENT: 'INVALID_EVENT_CONTENT',
+  INVALID_ADDRESS: 'INVALID_ADDRESS',
+  DEPOSIT_NOT_FOUND: 'DEPOSIT_NOT_FOUND',
+  INVALID_GAS_AMOUNT: 'INVALID_GAS_AMOUNT',
+  TRANSACTION_ERROR: 'TRANSACTION_ERROR',
+  ESTIMATION_ERROR: 'ESTIMATION_ERROR',
+  NO_ROUTE_FOUND: 'NO_ROUTE_FOUND',
+} as const;
 
-export type ErrorCode = (typeof ErrorCodesArray)[number];
+export type ErrorCode = (typeof ErrorCodes)[keyof typeof ErrorCodes];
 
-export const ErrorCode = Object.fromEntries(ErrorCodesArray.map((code) => [code, code])) as {
-  [K in ErrorCode]: K;
+export const ErrorCode = {
+  ...ErrorCodes,
+  isErrorCode: (code: string): code is ErrorCode => code in ErrorCodes,
 };
+
+// Type guard function
+export function isErrorCode(code: string): code is ErrorCode {
+  return code in ErrorCodes;
+}
