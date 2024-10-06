724b7b9e278b9fbc8016b9965a5208ac8ee2b95c
Adeyemi <addegbenga@gmail.com>
2024-10-06 03:10:40 +0100

    feat: wallet connect

--- apps/mobile/src/modules/WalletModal/index.tsx
+++ apps/mobile/src/modules/WalletModal/index.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import { useConnect } from '@starknet-react/core';
+import {AppKitButton, useAppKit} from '@reown/appkit-wagmi-react-native';
+import {useConnect} from '@starknet-react/core';
 import * as Linking from 'expo-linking';
-import { Platform, Pressable, View } from 'react-native';
-import { SvgXml } from 'react-native-svg';
+import React from 'react';
+import {Platform, Pressable, View} from 'react-native';
+import {SvgXml} from 'react-native-svg';
 
-import { Button, Modal, Text } from '../../components';
-import { ARGENT_X_INSTALL_URL, BRAAVOS_INSTALL_URL } from '../../constants/urls';
-import { useStyles, useTheme } from '../../hooks';
-import { useDialog } from '../../hooks/modals/useDialog';
+import {Button, Modal, Text} from '../../components';
+import {ARGENT_X_INSTALL_URL, BRAAVOS_INSTALL_URL} from '../../constants/urls';
+import {useEvmWallet} from '../../context/WalletModalEvmProvider';
+import {useStyles, useTheme} from '../../hooks';
+import {useDialog} from '../../hooks/modals/useDialog';
 import stylesheet from './styles';
-import { AppKitButton, useAppKit } from '@reown/appkit-wagmi-react-native';
 
 export type WalletModalProps = {
   hide: () => void;
 };
 
-export const WalletModal: React.FC<WalletModalProps> = ({ hide }) => {
-  const { theme } = useTheme();
+export const WalletModal: React.FC<WalletModalProps> = ({hide}) => {
+  const {theme} = useTheme();
   const styles = useStyles(stylesheet);
 
-  const { connect, connectors } = useConnect();
-  const { showDialog, hideDialog } = useDialog();
-  const { open: openEVM } = useAppKit()
+  const {connect, connectors} = useConnect();
+  const {showDialog, hideDialog} = useDialog();
+  const {open: openEVM} = useAppKit();
+  const {showEvmWallet} = useEvmWallet();
   return (
-    <Modal
-      style={{ zIndex: 100 }}
-    >
+    <Modal style={{zIndex: 100}}>
       <Text fontSize={16} weight="semiBold">
         Please choose a wallet to connect
       </Text>
@@ -65,7 +65,7 @@ export const WalletModal: React.FC<WalletModalProps> = ({ hide }) => {
                   hide();
                   return;
                 }
-                connect({ connector });
+                connect({connector});
                 hide();
               }}
               style={styles.connector}
@@ -80,17 +80,16 @@ export const WalletModal: React.FC<WalletModalProps> = ({ hide }) => {
 
       <View style={styles.connectors}>
         <Text>EVM</Text>
+        {/* <ConnectWalletScreen /> */}
         <Pressable
           style={styles.connector}
-
           onPress={() => {
-            console.log("openEvm")
-            openEVM()
-          }}>
+            showEvmWallet();
+            hide();
+          }}
+        >
           <Text>Connect EVM</Text>
         </Pressable>
-        {/* <AppKitButton /> */}
-
       </View>
 
       <Button variant="default" onPress={hide}>
