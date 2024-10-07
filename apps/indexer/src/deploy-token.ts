5fde983ba0f36cddb93277cdf6dc7e7655065675
MSghais <msghais135@gmail.com>
2024-10-07 16:50:48 +0200

    fix event keys indexer with the first _ as hash event name

--- apps/indexer/src/deploy-token.ts
+++ apps/indexer/src/deploy-token.ts
@@ -54,7 +54,7 @@ export default function DecodeTokenDeploy({ header, events }: Block) {
     if (!event.data || !event.keys) return;
 
     const transactionHash = transaction.meta.hash;
-    const [token_address, caller] = event.keys;
+    const [_, caller, token_address] = event.keys;
 
     const [
       symbol,
