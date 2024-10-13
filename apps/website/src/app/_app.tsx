91f8a6998367949dbe462719f98d93ebe762c178
MSghais <msghais135@gmail.com>
2024-10-14 01:03:23 +0200

    fix lint

--- apps/website/src/app/_app.tsx
+++ apps/website/src/app/_app.tsx
@@ -1,20 +1,20 @@
 import './index.css';
 import '@rainbow-me/rainbowkit/styles.css';
 
-import type { Metadata } from 'next';
+import type {Metadata} from 'next';
 export const metadata: Metadata = {
   title: 'afk community portal',
   description: 'afk community portal',
 };
-import { AppProps } from 'next/app';
+import {AppProps} from 'next/app';
+import {useRouter} from 'next/router';
+import Script from 'next/script';
+import {useEffect} from 'react';
 
 import Providers from './providers';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Script from 'next/script';
 const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS; // Replace with your actual tracking ID
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({Component, pageProps}: AppProps) {
   const router = useRouter();
 
   // Track page views
