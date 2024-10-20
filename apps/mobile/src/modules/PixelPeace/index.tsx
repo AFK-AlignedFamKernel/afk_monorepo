bfd9ff09025d6d6cb44b3414fcb0d57a4b442ceb
MSghais <msghais135@gmail.com>
2024-10-20 18:30:23 +0200

    add img tuto + css

--- apps/mobile/src/modules/PixelPeace/index.tsx
+++ apps/mobile/src/modules/PixelPeace/index.tsx
@@ -1,31 +1,39 @@
 import React from 'react';
-import {Platform, View} from 'react-native';
-// import { App, AppRender } from "pixel_ui"
+import { Platform, View } from 'react-native';
+import { App, AppRender } from "pixel_ui"
 import WebView from 'react-native-webview';
 
-import {useStyles} from '../../hooks';
-import {useIsDesktop} from '../../hooks/useIsDesktop';
+import { useStyles } from '../../hooks';
+import { useIsDesktop } from '../../hooks/useIsDesktop';
 import stylesheet from './styles';
+import { ART_PEACE_ADDRESS, USERNAME_STORE_ADDRESS } from 'common';
 
 export const PixelPeace: React.FC = () => {
   const styles = useStyles(stylesheet);
   const isDesktop = useIsDesktop();
 
   return (
-    <View style={{height: '100%'}}>
-      {Platform.OS == 'web' && process.env.EXPO_PUBLIC_PIXEL_URL && (
-        <>
-          <iframe
+    <View style={{ height: '100%' }}>
+      {Platform.OS == 'web'
+        // && process.env.EXPO_PUBLIC_PIXEL_URL 
+        && (
+          <>
+            <AppRender
+              artPeaceAddress={ART_PEACE_ADDRESS['0x534e5f5345504f4c4941']}
+              // nftCanvasAddress={}
+              usernameAddress={USERNAME_STORE_ADDRESS['0x534e5f5345504f4c4941']}
+            ></AppRender>
+            {/* <iframe
             src={process.env.EXPO_PUBLIC_PIXEL_URL}
             // height={isDesktop ? 750 : 550}
             height={550}
             width="100%"
-          ></iframe>
-        </>
-      )}
+          ></iframe> */}
+          </>
+        )}
 
       {Platform.OS != 'web' && process.env.EXPO_PUBLIC_PIXEL_URL && (
-        <WebView source={{uri: process.env.EXPO_PUBLIC_PIXEL_URL}}></WebView>
+        <WebView source={{ uri: process.env.EXPO_PUBLIC_PIXEL_URL }}></WebView>
       )}
     </View>
   );
