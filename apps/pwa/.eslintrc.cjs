e6e3e75e0a1a90b3291430176baaa4d1ed35ae14
MSGhais <msghais135@gmail.com>
2024-10-26 20:45:17 +0200

    fix build

--- apps/pwa/.eslintrc.cjs
+++ apps/pwa/.eslintrc.cjs
@@ -3,17 +3,18 @@ require('@uniswap/eslint-config/load');
 module.exports = {
   extends: ['next/core-web-vitals', '@uniswap/eslint-config/node'],
   rules: {
-    "@typescript-eslint/no-restricted-imports": [
-      "error",
-      {
-        paths: [
-          {
-            name: "ethers",
-            message: "Importing from 'ethers' is allowed for flexibility in this project.",
-          },
-        ],
-      },
-    ],
+    "@typescript-eslint/no-restricted-imports": "off",
+    // "@typescript-eslint/no-restricted-imports": [
+    //   "error",
+    //   {
+    //     paths: [
+    //       {
+    //         name: "ethers",
+    //         message: "Importing from 'ethers' is allowed for flexibility in this project.",
+    //       },
+    //     ],
+    //   },
+    // ],
     'import/no-unused-modules': 'off',
 
     'prettier/prettier': [
@@ -27,17 +28,18 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
       rules: {
-        "@typescript-eslint/no-restricted-imports": [
-          "error",
-          {
-            paths: [
-              {
-                name: "ethers",
-                message: "Importing from 'ethers' is allowed for flexibility in this project.",
-              },
-            ],
-          },
-        ],
+        // "@typescript-eslint/no-restricted-imports": [
+        //   "error",
+        //   {
+        //     paths: [
+        //       {
+        //         name: "ethers",
+        //         message: "Importing from 'ethers' is allowed for flexibility in this project.",
+        //       },
+        //     ],
+        //   },
+        // ],
+        "@typescript-eslint/no-restricted-imports": "off",
         'import/no-unused-modules': 'off',
 
         'prettier/prettier': [
--- apps/pwa/src/app/api/gift/receive/route.ts
+++ apps/pwa/src/app/api/gift/receive/route.ts
@@ -1,37 +1,12 @@
-import {Readable} from 'node:stream';
-
 import {NextRequest, NextResponse} from 'next/server';
 
-import {pinata} from '@/services/pinata';
 import {ErrorCode} from '@/utils/errors';
 import {HTTPStatus} from '@/utils/http';
 
 export async function POST(request: NextRequest) {
-  const data = await request.formData();
-  const requestBody = await request.json();
-
-  // const body = ClaimSchema.safeParse(requestBody);
-  // if (!body.success) {
-  //   return NextResponse.json(
-  //     {code: ErrorCode.BAD_REQUEST, error: body.error},
-  //     {status: HTTPStatus.BadRequest},
-  //   );
-  // }
-
   try {
-    // Verify
-    const stream = Readable.fromWeb(file.stream() as any);
-
-    const {IpfsHash} = await pinata.pinFileToIPFS(stream, {
-      pinataMetadata: {
-        name: file.name,
-      },
-    });
-
-    return NextResponse.json(
-      {hash: IpfsHash, url: `${process.env.IPFS_GATEWAY}/${IpfsHash}`},
-      {status: HTTPStatus.OK},
-    );
+    /** TODO Payment receive */
+    return NextResponse.json({message: 'lfg'}, {status: HTTPStatus.OK});
   } catch (error) {
     console.error(error);
 
