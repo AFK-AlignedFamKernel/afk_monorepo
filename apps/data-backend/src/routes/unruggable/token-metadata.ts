d3981f65d108c9983fb01dae8a56273a9d848dc0
EjembiEmmanuel <ejembioche50@gmail.com>
2024-11-20 16:53:11 +0100

    feat: add tokens metadata endpoint

---
+++ apps/data-backend/src/routes/unruggable/token-metadata.ts
@@ -0,0 +1,22 @@
+import type { FastifyInstance, RouteOptions } from "fastify";
+import { HTTPStatus } from "../../utils/http";
+
+async function tokenMetadataRoute(fastify: FastifyInstance, options: RouteOptions) {
+  fastify.get("/token-metadata", async (request, reply) => {
+    try {
+      const response = await fetch("https://unrug.top/api/data/1");
+      const data = await response.json();
+
+      const tokensMetadata = data.chart_data;
+
+      reply.status(HTTPStatus.OK).send({
+        data: tokensMetadata
+      });
+    } catch (error) {
+      console.error("Error fetching tokens metadata:", error);
+      reply.status(HTTPStatus.InternalServerError).send({ message: "Internal server error." });
+    }
+  });
+}
+
+export default tokenMetadataRoute;
