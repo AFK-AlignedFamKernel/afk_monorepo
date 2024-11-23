59c32e91fbe08efa093e5efef0a4e4ba5647cef0
MSGhais <msghais135@gmail.com>
2024-11-23 10:05:51 +0100

    one nameservice

--- apps/indexer/src/nameservice.ts
+++ apps/indexer/src/nameservice.ts
@@ -48,7 +48,7 @@ export const config = {
   sinkType: "postgres",
   sinkOptions: {
     connectionString: Deno.env.get("POSTGRES_CONNECTION_STRING"),
-    tableName: "token_deploy",
+    tableName: "username_claimed",
   },
 };
 
@@ -113,61 +113,61 @@ export default function DecodeUsernameClaimed({ header, events }: Block) {
 
 
 
-export function DecodeSubscriptionRenewed({ header, events }: Block) {
-  const { blockNumber, blockHash, timestamp } = header!;
+// export function DecodeSubscriptionRenewed({ header, events }: Block) {
+//   const { blockNumber, blockHash, timestamp } = header!;
 
-  return (events ?? []).map(({ event, transaction }) => {
-    if (!event.data || !event.keys) return;
+//   return (events ?? []).map(({ event, transaction }) => {
+//     if (!event.data || !event.keys) return;
 
-    const transactionHash = transaction.meta.hash;
-    const [_, address] = event.keys;
+//     const transactionHash = transaction.meta.hash;
+//     const [_, address] = event.keys;
 
-    const [
-      username,
-      expiry,
-      paid_high,
-      paid_low,
-      quote_token
-    ] = event.data;
+//     const [
+//       username,
+//       expiry,
+//       paid_high,
+//       paid_low,
+//       quote_token
+//     ] = event.data;
 
-    // const symbol_decoded = token_address
-    //   ? shortString.decodeShortString(symbol.replace(/0x0+/, "0x"))
-    //   : "";
+//     // const symbol_decoded = token_address
+//     //   ? shortString.decodeShortString(symbol.replace(/0x0+/, "0x"))
+//     //   : "";
 
-    // const name_decoded = name
-    //   ? shortString.decodeShortString(name.replace(/0x0+/, "0x"))
-    //   : "";
+//     // const name_decoded = name
+//     //   ? shortString.decodeShortString(name.replace(/0x0+/, "0x"))
+//     //   : "";
 
-    const paid_amount = uint256.uint256ToBN({
-      low: paid_low,
-      high: paid_high,
-    });
-    // const initial_supply = formatUnits(initial_supply_raw, DECIMALS).toString();
+//     const paid_amount = uint256.uint256ToBN({
+//       low: paid_low,
+//       high: paid_high,
+//     });
+//     // const initial_supply = formatUnits(initial_supply_raw, DECIMALS).toString();
 
-    // const total_supply_raw = uint256.uint256ToBN({
-    //   low: total_supply_low,
-    //   high: total_supply_high,
-    // });
-    // const total_supply = formatUnits(total_supply_raw, DECIMALS).toString();
+//     // const total_supply_raw = uint256.uint256ToBN({
+//     //   low: total_supply_low,
+//     //   high: total_supply_high,
+//     // });
+//     // const total_supply = formatUnits(total_supply_raw, DECIMALS).toString();
 
-    // console.log({
-    //   memecoin_address: token_address,
-    //   caller: caller,
-    // });
-    const amount_paid_formated = formatUnits(paid_amount, DECIMALS).toString();
+//     // console.log({
+//     //   memecoin_address: token_address,
+//     //   caller: caller,
+//     // });
+//     const amount_paid_formated = formatUnits(paid_amount, DECIMALS).toString();
 
-    return {
-      network: "starknet-sepolia",
-      block_hash: blockHash,
-      block_number: Number(blockNumber),
-      block_timestamp: timestamp,
-      transaction_hash: transactionHash,
-      owner_address: address,
-      name: username,
-      username: username,
-      expiry: expiry,
-      paid:amount_paid_formated,
-      created_at: new Date().toISOString(),
-    };
-  });
-}
+//     return {
+//       network: "starknet-sepolia",
+//       block_hash: blockHash,
+//       block_number: Number(blockNumber),
+//       block_timestamp: timestamp,
+//       transaction_hash: transactionHash,
+//       owner_address: address,
+//       name: username,
+//       username: username,
+//       expiry: expiry,
+//       paid:amount_paid_formated,
+//       created_at: new Date().toISOString(),
+//     };
+//   });
+// }
