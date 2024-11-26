1404bb3fe00c5bc2a369f6a2d968124c0601eebe
Adeyemi Gbenga <addegbenga@gmail.com>
2024-11-26 16:18:04 +0100

    fix: added twitter routes BE

--- apps/data-backend/src/router.ts
+++ apps/data-backend/src/router.ts
@@ -14,6 +14,8 @@ import getFunkitStripeCheckoutStatus from "./routes/funkit/get_funkit_stripe_che
 import createPaymentIntent from "./routes/stripe/createPaymentIntent";
 import paymentSheet from "./routes/stripe/paymentSheet";
 import { authRoutes } from "./routes/auth";
+import nameserviceRoutes from "./routes/indexer/nameservice/nameservice";
+import unrugRoutes from "./routes/indexer/unruggable";
 import twitterRoutes from "./routes/auth/twitter";
 // import getOtp from "./routes/otp/getOtp";
 // import verifyOtp from "./routes/otp/verifyOtp";
@@ -38,8 +40,10 @@ function declareRoutes(
   fastify.register(getFunkitStripeCheckoutStatus);
   fastify.register(createPaymentIntent);
   fastify.register(paymentSheet);
-  fastify.register(twitterRoutes);
   fastify.register(authRoutes);
+  fastify.register(unrugRoutes);
+  fastify.register(nameserviceRoutes);
+  fastify.register(twitterRoutes);
   // fastify.register(getOtp, twilio_services?.verifications);
   // fastify.register(verifyOtp, deployer, twilio_services?.verificationChecks);
   // fastify.register(verifyOtp, [deployer, twilio_services?.verificationChecks]);
