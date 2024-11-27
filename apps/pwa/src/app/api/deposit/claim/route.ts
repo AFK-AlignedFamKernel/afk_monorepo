92788cf4f0e1c8c32113e17d77ef4ab06263e815
Adeyemi Gbenga <addegbenga@gmail.com>
2024-11-27 22:44:43 +0100

    fix: pwa build issues

--- apps/pwa/src/app/api/deposit/claim/route.ts
+++ apps/pwa/src/app/api/deposit/claim/route.ts
@@ -1,10 +1,6 @@
-import {fetchBuildExecuteTransaction, fetchQuotes} from '@avnu/avnu-sdk';
 import {NextRequest, NextResponse} from 'next/server';
 import {Calldata} from 'starknet';
 
-import {ESCROW_ADDRESSES, ETH_ADDRESSES, STRK_ADDRESSES} from '@/constants/contracts';
-import {AVNU_URL, CHAIN_ID, Entrypoint} from '@/constants/misc';
-import {account} from '@/services/account';
 import {ErrorCode} from '@/utils/errors';
 import {HTTPStatus} from '@/utils/http';
 import {ClaimSchema} from '@/utils/validation';
@@ -38,90 +34,90 @@ export async function POST(request: NextRequest) {
     throw error;
   }
 
-  try {
-    if (
-      gasTokenAddress === ETH_ADDRESSES[CHAIN_ID] ||
-      gasTokenAddress === STRK_ADDRESSES[CHAIN_ID]
-    ) {
-      // ETH | STRK transaction
+  // try {
+  //   if (
+  //     gasTokenAddress === ETH_ADDRESSES[CHAIN_ID] ||
+  //     gasTokenAddress === STRK_ADDRESSES[CHAIN_ID]
+  //   ) {
+  //     // ETH | STRK transaction
 
-      const {transaction_hash} = await account.execute(
-        [
-          {
-            contractAddress: ESCROW_ADDRESSES[CHAIN_ID],
-            entrypoint: Entrypoint.CLAIM,
-            calldata: claimCallData,
-          },
-        ],
-        {
-          version: gasTokenAddress === ETH_ADDRESSES[CHAIN_ID] ? 1 : 3,
-          maxFee: gasAmount,
-        },
-      );
+  //     const {transaction_hash} = await account.execute(
+  //       [
+  //         {
+  //           contractAddress: ESCROW_ADDRESSES[CHAIN_ID],
+  //           entrypoint: Entrypoint.CLAIM,
+  //           calldata: claimCallData,
+  //         },
+  //       ],
+  //       {
+  //         version: gasTokenAddress === ETH_ADDRESSES[CHAIN_ID] ? 1 : 3,
+  //         maxFee: gasAmount,
+  //       },
+  //     );
 
-      return NextResponse.json({transaction_hash}, {status: HTTPStatus.OK});
-    } else {
-      // ERC20 transaction
+  //     return NextResponse.json({transaction_hash}, {status: HTTPStatus.OK});
+  //   } else {
+  //     // ERC20 transaction
 
-      const result = await account.estimateInvokeFee([
-        {
-          contractAddress: ESCROW_ADDRESSES[CHAIN_ID],
-          entrypoint: Entrypoint.CLAIM,
-          calldata: claimCallData,
-        },
-      ]);
+  //     const result = await account.estimateInvokeFee([
+  //       {
+  //         contractAddress: ESCROW_ADDRESSES[CHAIN_ID],
+  //         entrypoint: Entrypoint.CLAIM,
+  //         calldata: claimCallData,
+  //       },
+  //     ]);
 
-      const gasFeeQuotes = await fetchQuotes(
-        {
-          buyTokenAddress: ETH_ADDRESSES[CHAIN_ID],
-          sellTokenAddress: gasTokenAddress,
-          sellAmount: gasAmount,
-        },
-        {baseUrl: AVNU_URL},
-      );
-      const gasFeeQuote = gasFeeQuotes[0];
+  //     const gasFeeQuotes = await fetchQuotes(
+  //       {
+  //         buyTokenAddress: ETH_ADDRESSES[CHAIN_ID],
+  //         sellTokenAddress: gasTokenAddress,
+  //         sellAmount: gasAmount,
+  //       },
+  //       {baseUrl: AVNU_URL},
+  //     );
+  //     const gasFeeQuote = gasFeeQuotes[0];
 
-      if (!gasFeeQuote) {
-        return NextResponse.json({code: ErrorCode.NO_ROUTE_FOUND}, {status: HTTPStatus.BadRequest});
-      }
+  //     if (!gasFeeQuote) {
+  //       return NextResponse.json({code: ErrorCode.NO_ROUTE_FOUND}, {status: HTTPStatus.BadRequest});
+  //     }
 
-      if (result.overall_fee > gasFeeQuote.buyAmount) {
-        return NextResponse.json(
-          {code: ErrorCode.INVALID_GAS_AMOUNT},
-          {status: HTTPStatus.BadRequest},
-        );
-      }
+  //     if (result.overall_fee > gasFeeQuote.buyAmount) {
+  //       return NextResponse.json(
+  //         {code: ErrorCode.INVALID_GAS_AMOUNT},
+  //         {status: HTTPStatus.BadRequest},
+  //       );
+  //     }
 
-      const {calls: swapCalls} = await fetchBuildExecuteTransaction(
-        gasFeeQuote.quoteId,
-        account.address,
-        undefined,
-        undefined,
-        {baseUrl: AVNU_URL},
-      );
+  //     const {calls: swapCalls} = await fetchBuildExecuteTransaction(
+  //       gasFeeQuote.quoteId,
+  //       account.address,
+  //       undefined,
+  //       undefined,
+  //       {baseUrl: AVNU_URL},
+  //     );
 
-      const {transaction_hash} = await account.execute(
-        [
-          {
-            contractAddress: ESCROW_ADDRESSES[CHAIN_ID],
-            entrypoint: Entrypoint.CLAIM,
-            calldata: claimCallData,
-          },
-          ...swapCalls,
-        ],
-        {
-          maxFee: gasFeeQuote.buyAmount,
-        },
-      );
+  //     const {transaction_hash} = await account.execute(
+  //       [
+  //         {
+  //           contractAddress: ESCROW_ADDRESSES[CHAIN_ID],
+  //           entrypoint: Entrypoint.CLAIM,
+  //           calldata: claimCallData,
+  //         },
+  //         ...swapCalls,
+  //       ],
+  //       {
+  //         maxFee: gasFeeQuote.buyAmount,
+  //       },
+  //     );
 
-      return NextResponse.json({transaction_hash}, {status: HTTPStatus.OK});
-    }
-  } catch (error) {
-    console.error(error);
+  //     return NextResponse.json({transaction_hash}, {status: HTTPStatus.OK});
+  //   }
+  // } catch (error) {
+  //   console.error(error);
 
-    return NextResponse.json(
-      {code: ErrorCode.TRANSACTION_ERROR, error},
-      {status: HTTPStatus.InternalServerError},
-    );
-  }
+  //   return NextResponse.json(
+  //     {code: ErrorCode.TRANSACTION_ERROR, error},
+  //     {status: HTTPStatus.InternalServerError},
+  //   );
+  // }
 }
