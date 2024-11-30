2fdf4974d5f706419c72d6685910940eb4c92987
EjembiEmmanuel <ejembioche50@gmail.com>
2024-11-30 12:07:23 +0100

    feat: sell token indexer

--- apps/nestjs-indexer/src/indexer/indexer.module.ts
+++ apps/nestjs-indexer/src/indexer/indexer.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { IndexerService } from './indexer.service';
-import { BuyTokenIndexer } from './buy-token.indexer';
 import { TokenLaunchIndexer } from './token-launch.indexer';
-import { BuyTokenModule } from 'src/services/buy-token/buy-token.module';
+import { BuyTokenIndexer } from './buy-token.indexer';
+import { SellTokenIndexer } from './sell-token.indexer';
 import { TokenLaunchModule } from 'src/services/token-launch/token-launch.module';
+import { BuyTokenModule } from 'src/services/buy-token/buy-token.module';
+import { SellTokenModule } from 'src/services/sell-token/sell-token.module';
 
 @Module({
-  imports: [TokenLaunchModule, BuyTokenModule],
-  providers: [TokenLaunchIndexer, BuyTokenIndexer, IndexerService],
-  exports: [TokenLaunchIndexer, BuyTokenIndexer],
+  imports: [TokenLaunchModule, BuyTokenModule, SellTokenModule],
+  providers: [
+    TokenLaunchIndexer,
+    BuyTokenIndexer,
+    SellTokenIndexer,
+    IndexerService,
+  ],
+  exports: [TokenLaunchIndexer, BuyTokenIndexer, SellTokenIndexer],
 })
 export class IndexerModule {}
