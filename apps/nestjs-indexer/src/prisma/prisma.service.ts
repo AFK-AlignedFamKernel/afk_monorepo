8a64023ca984e8836b16927824d9e027a588306d
EjembiEmmanuel <ejembioche50@gmail.com>
2024-11-30 08:18:56 +0100

    feat: configure indexer prisma for nestjs indexer

--- apps/nestjs-indexer/src/prisma/prisma.service.ts
+++ apps/nestjs-indexer/src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from 'indexer-prisma';
 
 @Injectable()
 export class PrismaService
