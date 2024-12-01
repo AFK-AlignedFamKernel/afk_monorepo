76838ac4e96247ca984c4874ab9fcc3190e428ae
EjembiEmmanuel <ejembioche50@gmail.com>
2024-12-01 13:38:19 +0100

    feat: name service indexer

--- apps/nestjs-indexer/src/indexer/indexer.module.ts
+++ apps/nestjs-indexer/src/indexer/indexer.module.ts
@@ -4,10 +4,12 @@ import { TokenLaunchIndexer } from './token-launch.indexer';
 import { DeployTokenIndexer } from './deploy-token.indexer';
 import { BuyTokenIndexer } from './buy-token.indexer';
 import { SellTokenIndexer } from './sell-token.indexer';
+import { NameServiceIndexer } from './name-service.indexer';
 import { TokenLaunchModule } from 'src/services/token-launch/token-launch.module';
 import { DeployTokenModule } from 'src/services/deploy-token/deploy-token.module';
 import { BuyTokenModule } from 'src/services/buy-token/buy-token.module';
 import { SellTokenModule } from 'src/services/sell-token/sell-token.module';
+import { NameServiceModule } from 'src/services/name-service/name-service.module';
 
 @Module({
   imports: [
@@ -15,12 +17,14 @@ import { SellTokenModule } from 'src/services/sell-token/sell-token.module';
     DeployTokenModule,
     BuyTokenModule,
     SellTokenModule,
+    NameServiceModule,
   ],
   providers: [
     TokenLaunchIndexer,
     DeployTokenIndexer,
     BuyTokenIndexer,
     SellTokenIndexer,
+    NameServiceIndexer,
     IndexerService,
   ],
   exports: [
@@ -28,6 +32,7 @@ import { SellTokenModule } from 'src/services/sell-token/sell-token.module';
     DeployTokenIndexer,
     BuyTokenIndexer,
     SellTokenIndexer,
+    NameServiceIndexer,
   ],
 })
 export class IndexerModule {}
