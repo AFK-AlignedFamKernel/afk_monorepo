dc2e1a7278a0765fddfd377ad19f7ef2573d78fe
MSGhais <msghais135@gmail.com>
2024-12-10 20:51:01 +0100

    change index.ts import and dist used in package

---
+++ packages/indexer-prisma/src/index.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+export default prisma
+export { PrismaClient }
+
+// import { PrismaClient } from '@prisma/client'
+
+// export const prisma = new PrismaClient()
+
+// // Export any other types or utilities you need
+// export * from '@prisma/client'
