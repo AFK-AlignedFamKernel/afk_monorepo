dbe00fbd0425af6c04f55c4963d17bb7ab999db9
MSGhais <msghais135@gmail.com>
2024-12-10 21:34:23 +0100

    readd old import

--- apps/data-backend/src/routes/indexer/buy-coin.ts
+++ apps/data-backend/src/routes/indexer/buy-coin.ts
@@ -1,6 +1,6 @@
 import type { FastifyInstance, RouteOptions } from "fastify";
-// import prisma from "indexer-prisma"
-import {prisma} from "indexer-prisma"
+import prisma from "indexer-prisma"
+// import {prisma} from "indexer-prisma"
 
 import { HTTPStatus } from "../../utils/http";
 
