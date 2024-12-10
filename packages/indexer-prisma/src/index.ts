8a79ab50fdd0962413e073a95c2b29472fea1108
MSGhais <msghais135@gmail.com>
2024-12-10 21:47:46 +0100

    use dist and change js to ts

--- packages/indexer-prisma/src/index.ts
+++ packages/indexer-prisma/src/index.ts
@@ -1,6 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
+// Test the connection
+prisma.$connect()
+  .then(() => console.log('Successfully connected to database'))
+  .catch((e: any) => console.error('Failed to connect to database:', e))
+
 
 export default prisma
 export { PrismaClient, prisma }
