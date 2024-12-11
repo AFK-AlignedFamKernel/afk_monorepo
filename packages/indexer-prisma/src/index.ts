2a2a38625ab1f76505fdbb66e0bb31974316c5ae
MSGhais <msghais135@gmail.com>
2024-12-11 13:28:22 +0100

    add export *

--- packages/indexer-prisma/src/index.ts
+++ packages/indexer-prisma/src/index.ts
@@ -10,7 +10,7 @@ prisma.$connect()
 export * from '@prisma/client'
 
 export default prisma
-export { PrismaClient, prisma }
+export { PrismaClient }
 
 // import { PrismaClient } from '@prisma/client'
 
