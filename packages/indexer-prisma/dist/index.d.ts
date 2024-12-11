5d788f2220630b35a5cc0ffff07d8ce75267778a
MSGhais <msghais135@gmail.com>
2024-12-11 13:41:26 +0100

    try indexer prisma last build because two prisma in nodemodule

---
+++ packages/indexer-prisma/dist/index.d.ts
@@ -0,0 +1,6 @@
+import { PrismaClient } from '@prisma/client';
+declare const prisma: PrismaClient<import("@prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library").DefaultArgs>;
+export * from '@prisma/client';
+export default prisma;
+export { PrismaClient };
+//# sourceMappingURL=index.d.ts.map
--- packages/indexer-prisma/src/index.ts
+++ packages/indexer-prisma/src/index.ts
@@ -2,9 +2,9 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 // Test the connection
-prisma.$connect()
-  .then(() => console.log('Successfully connected to database'))
-  .catch((e: any) => console.error('Failed to connect to database:', e))
+// prisma.$connect()
+//   .then(() => console.log('Successfully connected to database'))
+//   .catch((e: any) => console.error('Failed to connect to database:', e))
 
 // Export everything from @prisma/client for types
 export * from '@prisma/client'
