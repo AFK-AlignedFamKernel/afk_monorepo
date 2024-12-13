667955966387d8c73d2a9c6aed7ff317e47b03fa
Nicolas Coquelet <nicolascoquelet@gmail.com>
2024-12-13 11:27:13 +0100

    feat: cleanup and fix prisma config

--- packages/indexer-prisma/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-export default prisma;
-declare const prisma: PrismaClient<
-  import("../prisma/.prisma/client").Prisma.PrismaClientOptions,
-  never,
-  import("../prisma/.prisma/client/runtime/library").DefaultArgs
->;
-import { PrismaClient } from "../prisma/.prisma/client";
-export { PrismaClient };
--- packages/indexer-prisma/src/index.ts
+++ packages/indexer-prisma/src/index.ts
@@ -1,20 +1,6 @@
-import {PrismaClient} from '../prisma/.prisma/client'
+import { PrismaClient } from '../prisma/.prisma/client';
 
-const prisma = new PrismaClient()
-// Test the connection
-// prisma.$connect()
-//   .then(() => console.log('Successfully connected to database'))
-//   .catch((e: any) => console.error('Failed to connect to database:', e))
+const prisma = new PrismaClient();
 
-// Export everything from @prisma/client for types
-export * from '@prisma/client'
-
-export default prisma
-export { PrismaClient }
-
-// import { PrismaClient } from '@prisma/client'
-
-// export const prisma = new PrismaClient()
-
-// // Export any other types or utilities you need
-// export * from '@prisma/client'
+export default prisma;
+export { PrismaClient };
