ce6e0d4459f6f31ab020b2b9531fa41c75a8ed88
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2024-12-14 21:20:37 +0100

    made fixes

--- packages/afk_nostr_sdk/src/utils/relay.ts
+++ packages/afk_nostr_sdk/src/utils/relay.ts
@@ -4,11 +4,13 @@ export const RELAYS_PROD = [
   'wss://relay.nostr.band',
   'wss://purplepag.es',
   'wss://relay.snort.social',
-  'wss://nos.lol',
-  // 'wss://relay.n057r.club', 'wss://relay.nostr.net',
+  'wss://nos.lol'
 ];
 
-export const RELAYS_TEST = ['wss://relay.n057r.club', 'wss://relay.nostr.net'];
+export const RELAYS_TEST = [
+  'wss://relay.damus.io',
+  'wss://nos.lol'
+];
 
 export const RELAY_AFK_PRODUCTION = 'wss://nostr-relay-nestjs-production.up.railway.app';
 
@@ -19,14 +21,13 @@ export const AFK_RELAYS =
     ? [
         'wss://nostr-relay-nestjs-production.up.railway.app',
         ...RELAYS_PROD,
-        'wss://nostr-relay-nestjs-production.up.railway.app',
       ]
     : [
         'wss://nostr-relay-nestjs-production.up.railway.app',
-        ...RELAYS_TEST,
+        ...RELAYS_TEST
       ];
 
-// export const AFK_RELAYS = [
+      // export const AFK_RELAYS = [
 //   'wss://nostr.joyboy.community',
 //   // 'ws://localhost:3000', // comment if you don't run a relayer in localhost
 // ];
