7856e448131d2cd480c443607317ef3f610a7289
Nicolas Coquelet <nicolascoquelet@gmail.com>
2024-12-16 10:09:30 +0100

    fix: fix husky format config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,6 @@ module.exports = {
   '*.{js,ts,tsx,json,md,mdx,html,css,scss,yml}': 'prettier --write',
   '*.sol': 'prettier --write --plugin=prettier-plugin-solidity',
   '*.cairo': 'scarb fmt',
-  '*.prisma': 'prisma format',
+  '*.prisma': (stagedFiles) => stagedFiles.map((schema) => `prisma format --schema ${schema}`),
   '*.go': 'go fmt',
 };
