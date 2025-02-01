189ed232ee91fa463d36f0b4588532711854961d
Nicolas Coquelet <nicolascoquelet@gmail.com>
2025-02-02 00:12:54 +0100

    fix: add missing tx_hash

--- apps/data-backend/src/routes/indexer/tip.ts
+++ apps/data-backend/src/routes/indexer/tip.ts
@@ -1,8 +1,8 @@
-import type {FastifyInstance, RouteOptions} from 'fastify';
+import type { FastifyInstance, RouteOptions } from 'fastify';
 import prisma from 'indexer-prisma';
-import {HTTPStatus} from '../../utils/http';
-import {isValidStarknetAddress} from '../../utils/starknet';
-import {isValidNostrAddress} from "../../utils/nostr";
+import { HTTPStatus } from '../../utils/http';
+import { isValidStarknetAddress } from '../../utils/starknet';
+import { isValidNostrAddress } from '../../utils/nostr';
 
 interface TipParams {
   deposit_id?: string;
@@ -16,6 +16,7 @@ async function tipServiceRoute(fastify: FastifyInstance, options: RouteOptions) 
     try {
       const tips = await prisma.tip_deposit.findMany({
         select: {
+          transaction_hash: true,
           deposit_id: true,
           sender: true,
           nostr_recipient: true,
@@ -49,6 +50,7 @@ async function tipServiceRoute(fastify: FastifyInstance, options: RouteOptions) 
       const tip = await prisma.tip_deposit.findUnique({
         where: { deposit_id },
         select: {
+          transaction_hash: true,
           deposit_id: true,
           sender: true,
           nostr_recipient: true,
@@ -89,6 +91,7 @@ async function tipServiceRoute(fastify: FastifyInstance, options: RouteOptions) 
       const tips = await prisma.tip_deposit.findMany({
         where: { sender },
         select: {
+          transaction_hash: true,
           deposit_id: true,
           sender: true,
           nostr_recipient: true,
@@ -145,25 +148,27 @@ async function tipServiceRoute(fastify: FastifyInstance, options: RouteOptions) 
         },
       });
 
-      const tipsTransfer = (await prisma.tip_transfer.findMany({
-        where: { nostr_recipient },
-        select: {
-          transaction_hash: true,
-          sender: true,
-          nostr_recipient: true,
-          starknet_recipient: true,
-          token_address: true,
-          amount: true,
-          created_at: true,
-          updated_at: true,
-        },
-      })).map(transfer => ({
+      const tipsTransfer = (
+        await prisma.tip_transfer.findMany({
+          where: { nostr_recipient },
+          select: {
+            transaction_hash: true,
+            sender: true,
+            nostr_recipient: true,
+            starknet_recipient: true,
+            token_address: true,
+            amount: true,
+            created_at: true,
+            updated_at: true,
+          },
+        })
+      ).map((transfer) => ({
         ...transfer,
         is_claimed: true,
       }));
 
       reply.status(HTTPStatus.OK).send({
-        data: [ ...tipsDeposit, ...tipsTransfer ],
+        data: [...tipsDeposit, ...tipsTransfer],
       });
     } catch (error) {
       console.error('Error fetching tips by recipient:', error);
