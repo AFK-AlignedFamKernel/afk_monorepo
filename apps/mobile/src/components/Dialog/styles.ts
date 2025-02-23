c3d02e94453c94b03a0f8b75ef43406d8c3b8a0e
Elite-tch <149904417+Elite-tch@users.noreply.github.com>
2025-02-23 20:30:17 +0100

    login height updated

--- apps/mobile/src/components/Dialog/styles.ts
+++ apps/mobile/src/components/Dialog/styles.ts
@@ -1,20 +1,19 @@
 import {Spacing, ThemedStyleSheet} from '../../styles';
 
 export default ThemedStyleSheet((theme) => ({
-  modal: {
+ modal: {
     padding: Spacing.large,
     height: '90vh', // 90% of the viewport height
     width: '70vw', // 70% of the viewport width
-    maxWidth: '600', // Prevents it from being too wide on large screens
+    maxWidth: '600px', // Prevents it from being too wide on large screens
     maxHeight: '500px', // Prevents it from being too tall
-    minWidth: '350px', // Ensures it doesn't get too small
+    minWidth: '350px',  // Ensures it doesn't get too small
     minHeight: '200px', // Ensures it remains readable
-    paddingTop: 25px; // Choose a value that makes sense
-
-  },
+  },  
 
   content: {
     alignItems: 'center',
+    paddingTop: 100,
   },
   icon: {
     marginTop: Spacing.xsmall,
@@ -25,7 +24,9 @@ export default ThemedStyleSheet((theme) => ({
   },
 
   buttons: {
-    gap: Spacing.xsmall,
-    marginTop: Spacing.xlarge,
+    gap: Spacing.large,
+    marginTop: Spacing.xxlarge,
+    width: '60%',
+    margin: 'auto'
   },
 }));
