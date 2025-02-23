bc3b26bcb4c074215ef51af64e22bb3945f48a86
Elite-tch <149904417+Elite-tch@users.noreply.github.com>
2025-02-23 20:27:13 +0100

    login popup modal height

--- apps/mobile/src/components/Dialog/styles.ts
+++ apps/mobile/src/components/Dialog/styles.ts
@@ -3,6 +3,14 @@ import {Spacing, ThemedStyleSheet} from '../../styles';
 export default ThemedStyleSheet((theme) => ({
   modal: {
     padding: Spacing.large,
+    height: '90vh', // 90% of the viewport height
+    width: '70vw', // 70% of the viewport width
+    maxWidth: '600', // Prevents it from being too wide on large screens
+    maxHeight: '500px', // Prevents it from being too tall
+    minWidth: '350px', // Ensures it doesn't get too small
+    minHeight: '200px', // Ensures it remains readable
+    paddingTop: 25px; // Choose a value that makes sense
+
   },
 
   content: {
