a71700aff90085b5eeb75ef17ec1afc480dfa49b
MSG <59928086+MSghais@users.noreply.github.com>
2025-02-26 02:07:16 +0700

    Ui sprint (#534)
    * check indexer var
    
    * more recent default value with 1h
    
    * fix short button for both view and video limit and search
    
    * refetch and loading
    
    * refacto and fix etc
    
    * price fix

--- apps/mobile/src/components/NostrVideo/styles.ts
+++ apps/mobile/src/components/NostrVideo/styles.ts
@@ -5,7 +5,8 @@ import {ThemedStyleSheet} from '../../styles';
 export default ThemedStyleSheet(() => ({
   videoContainer: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height - 60,
+    // height: Dimensions.get('window').height - 60,
+    height: Dimensions.get('window').height,
   },
   video: {
     width: '100%',
@@ -13,15 +14,38 @@ export default ThemedStyleSheet(() => ({
   },
   innerVideo: {
     width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height - 60,
+    // height: Dimensions.get('window').height - 60,
+    height: Dimensions.get('window').height,
   },
   actionsContainer: {
-    position: 'absolute',
-    right: 20,
-    bottom: 40,
+    position: 'relative',
+    right: Dimensions.get('window').width * 0.1,
+    bottom: Dimensions.get('window').height * 0.1,
+    // right: Dimensions.get('window').width * 0.2,
+    // bottom: Dimensions.get('window').height * 0.2,
     display: 'flex',
     flexDirection: 'column',
     gap: 20,
     alignItems: 'center',
   },
+  actionsContainerMobile: {
+    position: 'absolute',
+    right: 20,
+    // bottom: 20,
+    // right: Dimensions.get('window').width * 0.2,
+    bottom: Dimensions.get('window').height * 0.2,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 20,
+    alignItems: 'center',
+  },
+  // actionsContainer: {
+  //   position: 'absolute',
+  //   right: 20,
+  //   bottom: 40,
+  //   display: 'flex',
+  //   flexDirection: 'column',
+  //   gap: 20,
+  //   alignItems: 'center',
+  // },
 }));
