d2ec82cbb388efae4cc6d6b3d073930cf1472243
nicode <nicolascoquelet@gmail.com>
2025-02-27 00:32:05 +0100

    feat: Dao screen

--- apps/mobile/src/modules/Dao/index.tsx
+++ apps/mobile/src/modules/Dao/index.tsx
@@ -1,11 +1,12 @@
-import { useNavigation } from '@react-navigation/native';
-import { useState } from 'react';
-import { View, Text } from 'react-native';
-import { useStyles, useTheme } from '../../hooks';
-import { MainStackNavigationProps } from '../../types';
-import { SelectedTab, TABS_COMMUNITY, TABS_TIP_LIST } from '../../types/tab';
+import {useNavigation} from '@react-navigation/native';
+import {useState} from 'react';
+import {Text, View} from 'react-native';
+
+import {Button, Input, Modal} from '../../components';
+import {useStyles, useTheme} from '../../hooks';
+import {MainStackNavigationProps} from '../../types';
+import {SelectedTab} from '../../types/tab';
 import stylesheet from './styles';
-import { Button, Input, Modal } from '../../components';
 
 export const DAOComponent: React.FC = () => {
   const styles = useStyles(stylesheet);
@@ -19,37 +20,26 @@ export const DAOComponent: React.FC = () => {
     }
   };
 
-
   const [isFormCreateDao, setIsFormCreateDao] = useState(false);
   const handleFormCreateDao = () => {
-
     setIsFormCreateDao(!isFormCreateDao);
     // navigation.navigate('MainStack', { screen: 'CreateForm' });
-  }
+  };
 
-  const handleCallbackCreateDao = () => {
-
-  }
+  const handleCallbackCreateDao = () => {};
   return (
     <View style={styles.container}>
-
       {isFormCreateDao && (
         <View>
           <Text style={styles.text}>Create DAO</Text>
           <Modal
-            style={{ height: "80%" }}
-          // style={styles.modal}
+            style={{height: '80%'}}
+            // style={styles.modal}
           >
             <View style={styles.modalContent}>
+              <Input></Input>
 
-
-              <Input>
-              </Input>
-
-
-              <Button
-                onPress={handleCallbackCreateDao}
-              >
+              <Button onPress={handleCallbackCreateDao}>
                 <Text>Create</Text>
               </Button>
             </View>
@@ -57,13 +47,12 @@ export const DAOComponent: React.FC = () => {
         </View>
       )}
 
-     <View style={styles.modalContents} >
-     <Text style={styles.text}>DAO coming soon</Text>
-<Button
-  onPress={handleFormCreateDao}
->
-  <Text>Create DAO</Text>
-  </Button>  </View>
+      <View style={styles.modalContents}>
+        <Text style={styles.text}>DAO coming soon</Text>
+        <Button onPress={handleFormCreateDao}>
+          <Text>Create DAO</Text>
+        </Button>{' '}
+      </View>
     </View>
   );
 };
---
+++ apps/mobile/src/screens/DAO/DaoComponent.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { FlatList, Modal, RefreshControl, ScrollView, Text, View } from 'react-native';
+
+import { Button } from '../../components';
+import Loading from '../../components/Loading';
+import { useStyles } from '../../hooks';
+import { useDaoList } from '../../hooks/api/indexer/useDaos';
+import { useDimensions } from '../../hooks/useWindowDimensions';
+import { DaoCard } from '../../modules/Dao/DaoCard';
+import { CreateDaoModal } from '../../modules/Dao/DaoCreationModal';
+import stylesheet from './styles';
+
+export const DAOComponent: React.FC = () => {
+  const styles = useStyles(stylesheet);
+
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const { isDesktop } = useDimensions();
+
+  const { data: daoList, isLoading, isFetching } = useDaoList();
+
+  return (
+    <View style={styles.container}>
+      <Button
+        onPress={() => setModalVisible(true)}
+        variant="primary"
+        style={styles.createDaoButton}
+        textStyle={styles.createDaoButtonText}
+      >
+        <Text>Create your DAO</Text>
+      </Button>
+
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={isModalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <CreateDaoModal handleModal={() => setModalVisible(false)} />
+      </Modal>
+
+      <ScrollView>
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <ScrollView
+            showsHorizontalScrollIndicator={false}
+            showsVerticalScrollIndicator={false}
+            style={{ flex: 1, padding: 5 }}
+          >
+            <FlatList
+              contentContainerStyle={styles.flatListContent}
+              data={daoList}
+              keyExtractor={(item) => item.contractAddress}
+              key={`flatlist-${isDesktop ? 3 : 1}`}
+              numColumns={isDesktop ? 3 : 1}
+              renderItem={({ item }) => {
+                return <DaoCard key={item.contractAddress} dao={item} />;
+              }}
+              refreshControl={<RefreshControl refreshing={isFetching} />}
+            />
+          </ScrollView>
+        )}
+      </ScrollView>
+    </View>
+  );
+};
