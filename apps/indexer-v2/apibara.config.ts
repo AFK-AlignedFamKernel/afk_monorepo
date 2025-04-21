93327ce3d00d9e842e8f2d5eaa947378b8e4a88c
MSGhais <msghais135@gmail.com>
2025-04-21 18:11:13 +0200

    wip weird issue namespace

--- apps/indexer-v2/apibara.config.ts
+++ apps/indexer-v2/apibara.config.ts
@@ -7,12 +7,12 @@ dotenv.config();
 export default defineConfig({
   runtimeConfig: {
     streamUrl: 'https://starknet-sepolia.preview.apibara.org',
-    startingBlock: process.env.STARTING_BLOCK ? parseInt(process.env.STARTING_BLOCK) : 705340,
-    // startingCursor: {
-    //   orderKey: process.env.ORDER_KEY ? parseInt(process.env.ORDER_KEY) : 705340,
-    //   // orderKey: 500000,
-    //   // orderKey: 533390,
-    // },
+    startingBlock: process.env.STARTING_BLOCK ? parseInt(process.env.STARTING_BLOCK) : 705040,
+    startingCursor: {
+      orderKey: process.env.ORDER_KEY ? parseInt(process.env.ORDER_KEY) : 705040,
+      // orderKey: 500000,
+      // orderKey: 533390,
+    },
     pgLiteDBPath: 'memory://persistence',
   },
   presets: {
--- apps/indexer-v2/indexers/namespace.indexer.ts
+++ apps/indexer-v2/indexers/namespace.indexer.ts
@@ -1,6 +1,6 @@
 import { defineIndexer } from '@apibara/indexer';
 import { useLogger } from '@apibara/indexer/plugins';
-import { drizzleStorage } from '@apibara/plugin-drizzle';
+import { drizzleStorage, useDrizzleStorage } from '@apibara/plugin-drizzle';
 import { Abi, decodeEvent, StarknetStream } from '@apibara/starknet';
 import { constants, encode, hash } from 'starknet';
 import { ApibaraRuntimeConfig } from 'apibara/types';
@@ -25,23 +25,27 @@ const KNOWN_EVENT_KEYS = [
   PUSH_ALGO_SCORE,
   ADMIN_ADD_NOSTR_PROFILE,
 ]
-export default function (config: ApibaraRuntimeConfig & { 
+export default function (config: ApibaraRuntimeConfig & {
   startingBlock: number,
-  // startingCursor: { orderKey: string } 
+  startingCursor: { orderKey: string }
 }) {
   console.log("config", config.startingBlock);
+  const {
+    startingBlock,
+  } = config;
+
   return defineIndexer(StarknetStream)({
     streamUrl: config.streamUrl as string,
-    startingBlock: BigInt(config.startingBlock ?? 533390),
+    startingBlock: BigInt(startingBlock ?? 533390),
     // startingCursor: {
-    //   orderKey: BigInt(config.startingCursor?.orderKey ?? 533390),
+    //   orderKey: BigInt(config?.startingCursor?.orderKey ?? 533390),
     //   // blockHash: config.startingCursor.blockHash,
     //   // orderKey: config.startingCursor.orderKey,
     // },
     filter: {
       events: [
         {
-          address: "0x07607c8A50b83938ea3f9DA25DC1b7024814C0E5bF4B40bF6D6FF9Bc7387aa7d",
+          address: "0x07607c8A50b83938ea3f9DA25DC1b7024814C0E5bF4B40bF6D6FF9Bc7387aa7d" as `0x${string}`,
           keys: [
             LINKED_ADDRESS,
             PUSH_ALGO_SCORE,
@@ -64,103 +68,198 @@ export default function (config: ApibaraRuntimeConfig & {
 
       // idColumn: 'id'
     })],
-    async transform({ block }) {
-      try {
-        const logger = useLogger();
-        const { events, header } = block;
-        console.log("transform started");
-        console.log("block", header?.blockNumber);
-        console.log("events length", events?.length);
-        // Save cursor at the start
-        // await saveCursor(header.blockNumber.toString(), header?.blockHash as string);
+    async transform({ endCursor, block, context, finality, header }) {
+      const logger = useLogger();
+      const { db } = useDrizzleStorage();
+      const { events } = block;
 
-        if (events.length === 0) {
-          return;
+      logger.info(
+        "Transforming block | orderKey: ",
+        endCursor?.orderKey,
+        " | finality: ",
+        finality,
+      );
+
+      const transactionHashes = new Set<string>();
+      console.log("block", header?.blockNumber);
+      console.log("events length", events?.length);
+
+      for (const event of events) {
+        if (event.transactionHash) {
+          transactionHashes.add(event.transactionHash);
         }
+        logger.log(`Found event ${event.keys[0]}`);
 
-        for (const event of events) {
-          logger.log(`Found event ${event.keys[0]}`);
+        try {
+          let sanitizedEventKey = encode.sanitizeHex(event.keys[0]);
+          console.log("event.keys[0]", event.keys[0]);
+          let decodedEvent: any;
 
-          // // Log unknown events instead of failing
-          // if (!KNOWN_EVENT_KEYS.includes(event.keys[0])) {
-          //   logger.warn(`Skipping unknown event key: ${event.keys[0]}`);
-          //   continue;
-          // }
+          if (event.keys[0] == encode.sanitizeHex(LINKED_ADDRESS)) {
+            console.log("LINKED_ADDRESS", event);
 
+            const decodedEvent = decodeEvent({
+              abi: namespaceABI as Abi,
+              event,
+              // eventName: 'afk::interfaces::common_interfaces::LinkedDefaultStarknetAddressEvent',
+              eventName: 'afk::interfaces::namespace::LinkedDefaultStarknetAddressEvent',
+            });
+            await handleLinkedAddressEvent(decodedEvent, event.address);
+          } else if (event.keys[0] == encode.sanitizeHex(ADMIN_ADD_NOSTR_PROFILE)) {
+            console.log("ADMIN_ADD_NOSTR_PROFILE", event);
 
-          try {
-            let sanitizedEventKey = encode.sanitizeHex(event.keys[0]);
-            console.log("event.keys[0]", event.keys[0]);
-            let decodedEvent: any;
-
-            if (event.keys[0] == encode.sanitizeHex(LINKED_ADDRESS)) {
-              console.log("LINKED_ADDRESS", event);
-
-              const decodedEvent = decodeEvent({  
-                abi: namespaceABI as Abi,
-                event,
-                eventName: 'afk::interfaces::namespace::LinkedDefaultStarknetAddressEvent',
-              });
-              await handleLinkedAddressEvent(decodedEvent, event.address);
-            } else if (event.keys[0] == encode.sanitizeHex(ADMIN_ADD_NOSTR_PROFILE)) {
-              console.log("ADMIN_ADD_NOSTR_PROFILE", event);
-
-              const decodedEvent = decodeEvent({
-                abi: namespaceABI as Abi,
-                event,
-                eventName: 'afk::interfaces::nostrfi_scoring_interfaces::AdminAddNostrProfile',
-              });
-              await handleAdminLinkedAddressEvent(decodedEvent, event.address);
-            } else if (event.keys[0] == encode.sanitizeHex(PUSH_ALGO_SCORE)) {
-              console.log("PUSH_ALGO_SCORE", event);
-              const decodedEvent = decodeEvent({
-                abi: namespaceABI as Abi,
-                event,
-                eventName: 'afk::interfaces::nostrfi_scoring_interfaces::PushAlgoScoreEvent',
-              });
-              await handlePushAlgoScoreEvent(decodedEvent, event.address);
-            }
-
-            // switch (encode.sanitizeHex(event.keys[0])) {
-            // switch (event.keys[0]) {
-            //   case NEW_EPOCH:
-            //     console.log("NEW_EPOCH", decodedEvent);
-            //     await handleNewEpochEvent(decodedEvent, event.address);
-            //     break;
-            //   case DEPOSIT_REWARDS:
-            //     console.log("DEPOSIT_REWARDS", decodedEvent);
-            //     await handleDepositRewardsEvent(decodedEvent, event.address);
-            //     break;
-            //   case DISTRIBUTION_REWARDS:
-            //     await handleDistributionRewardsEvent(decodedEvent, event.address);
-            //     break;
-            //   case TIP_USER:
-            //     console.log("TIP_USER", decodedEvent);
-            //     await handleTipUserEvent(decodedEvent, event.address);
-            //     break;
-            //   case LINKED_ADDRESS:
-            //     await handleLinkedAddressEvent(decodedEvent, event.address);
-            //     break;
-            //   case PUSH_ALGO_SCORE:
-            //     await handlePushAlgoScoreEvent(decodedEvent, event.address);
-            //     break;
-            //   case ADD_TOPICS:
-            //     await handleAddTopicsEvent(decodedEvent, event.address);
-            //     break;
-            //   case NOSTR_METADATA:
-            //     await handleNostrMetadataEvent(decodedEvent, event.address);
-            //     break;
-            // }
-          } catch (error: any) {
-            logger.error(`Error processing event: ${error.message}`);
+            const decodedEvent = decodeEvent({
+              abi: namespaceABI as Abi,
+              event,
+              eventName: 'afk::interfaces::nostrfi_scoring_interfaces::AdminAddNostrProfile',
+            });
+            await handleAdminLinkedAddressEvent(decodedEvent, event.address);
+          } else if (event.keys[0] == encode.sanitizeHex(PUSH_ALGO_SCORE)) {
+            console.log("PUSH_ALGO_SCORE", event);
+            const decodedEvent = decodeEvent({
+              abi: namespaceABI as Abi,
+              event,
+              eventName: 'afk::interfaces::nostrfi_scoring_interfaces::PushAlgoScoreEvent',
+            });
+            await handlePushAlgoScoreEvent(decodedEvent, event.address);
           }
+
+          // switch (encode.sanitizeHex(event.keys[0])) {
+          // switch (event.keys[0]) {
+          //   case NEW_EPOCH:
+          //     console.log("NEW_EPOCH", decodedEvent);
+          //     await handleNewEpochEvent(decodedEvent, event.address);
+          //     break;
+          //   case DEPOSIT_REWARDS:
+          //     console.log("DEPOSIT_REWARDS", decodedEvent);
+          //     await handleDepositRewardsEvent(decodedEvent, event.address);
+          //     break;
+          //   case DISTRIBUTION_REWARDS:
+          //     await handleDistributionRewardsEvent(decodedEvent, event.address);
+          //     break;
+          //   case TIP_USER:
+          //     console.log("TIP_USER", decodedEvent);
+          //     await handleTipUserEvent(decodedEvent, event.address);
+          //     break;
+          //   case LINKED_ADDRESS:
+          //     await handleLinkedAddressEvent(decodedEvent, event.address);
+          //     break;
+          //   case PUSH_ALGO_SCORE:
+          //     await handlePushAlgoScoreEvent(decodedEvent, event.address);
+          //     break;
+          //   case ADD_TOPICS:
+          //     await handleAddTopicsEvent(decodedEvent, event.address);
+          //     break;
+          //   case NOSTR_METADATA:
+          //     await handleNostrMetadataEvent(decodedEvent, event.address);
+          //     break;
+          // }
+        } catch (error: any) {
+          logger.error(`Error processing event: ${error.message}`);
         }
-      } catch (error) {
-        console.log("error transform", error);
       }
 
-    },
-  });
+    }
+    // async transform({ block }) {
+    //   try {
+    //     console.log("transform started");
+
+    //     const logger = useLogger();
+    //     const { events, header } = block;
+    //     console.log("block", header?.blockNumber);
+    //     console.log("events length", events?.length);
+    //     // Save cursor at the start
+    //     // await saveCursor(header.blockNumber.toString(), header?.blockHash as string);
+
+    //     if (events.length === 0) {
+    //       return;
+    //     }
+
+    //     for (const event of events) {
+    //       logger.log(`Found event ${event.keys[0]}`);
+
+    //       // // Log unknown events instead of failing
+    //       // if (!KNOWN_EVENT_KEYS.includes(event.keys[0])) {
+    //       //   logger.warn(`Skipping unknown event key: ${event.keys[0]}`);
+    //       //   continue;
+    //       // }
+
+
+    //       try {
+    //         let sanitizedEventKey = encode.sanitizeHex(event.keys[0]);
+    //         console.log("event.keys[0]", event.keys[0]);
+    //         let decodedEvent: any;
+
+    //         if (event.keys[0] == encode.sanitizeHex(LINKED_ADDRESS)) {
+    //           console.log("LINKED_ADDRESS", event);
+
+    //           const decodedEvent = decodeEvent({
+    //             abi: namespaceABI as Abi,
+    //             event,
+    //             eventName: 'afk::interfaces::common_interfaces::LinkedDefaultStarknetAddressEvent',
+    //             // eventName: 'afk::interfaces::namespace::LinkedDefaultStarknetAddressEvent',
+    //             // eventName: 'afk::interfaces::namespace::LinkedDefaultStarknetAddressEvent',
+    //           });
+    //           await handleLinkedAddressEvent(decodedEvent, event.address);
+    //         } else if (event.keys[0] == encode.sanitizeHex(ADMIN_ADD_NOSTR_PROFILE)) {
+    //           console.log("ADMIN_ADD_NOSTR_PROFILE", event);
+
+    //           const decodedEvent = decodeEvent({
+    //             abi: namespaceABI as Abi,
+    //             event,
+    //             eventName: 'afk::interfaces::nostrfi_scoring_interfaces::AdminAddNostrProfile',
+    //           });
+    //           await handleAdminLinkedAddressEvent(decodedEvent, event.address);
+    //         } else if (event.keys[0] == encode.sanitizeHex(PUSH_ALGO_SCORE)) {
+    //           console.log("PUSH_ALGO_SCORE", event);
+    //           const decodedEvent = decodeEvent({
+    //             abi: namespaceABI as Abi,
+    //             event,
+    //             eventName: 'afk::interfaces::nostrfi_scoring_interfaces::PushAlgoScoreEvent',
+    //           });
+    //           await handlePushAlgoScoreEvent(decodedEvent, event.address);
+    //         }
+
+    //         // switch (encode.sanitizeHex(event.keys[0])) {
+    //         // switch (event.keys[0]) {
+    //         //   case NEW_EPOCH:
+    //         //     console.log("NEW_EPOCH", decodedEvent);
+    //         //     await handleNewEpochEvent(decodedEvent, event.address);
+    //         //     break;
+    //         //   case DEPOSIT_REWARDS:
+    //         //     console.log("DEPOSIT_REWARDS", decodedEvent);
+    //         //     await handleDepositRewardsEvent(decodedEvent, event.address);
+    //         //     break;
+    //         //   case DISTRIBUTION_REWARDS:
+    //         //     await handleDistributionRewardsEvent(decodedEvent, event.address);
+    //         //     break;
+    //         //   case TIP_USER:
+    //         //     console.log("TIP_USER", decodedEvent);
+    //         //     await handleTipUserEvent(decodedEvent, event.address);
+    //         //     break;
+    //         //   case LINKED_ADDRESS:
+    //         //     await handleLinkedAddressEvent(decodedEvent, event.address);
+    //         //     break;
+    //         //   case PUSH_ALGO_SCORE:
+    //         //     await handlePushAlgoScoreEvent(decodedEvent, event.address);
+    //         //     break;
+    //         //   case ADD_TOPICS:
+    //         //     await handleAddTopicsEvent(decodedEvent, event.address);
+    //         //     break;
+    //         //   case NOSTR_METADATA:
+    //         //     await handleNostrMetadataEvent(decodedEvent, event.address);
+    //         //     break;
+    //         // }
+    //       } catch (error: any) {
+    //         logger.error(`Error processing event: ${error.message}`);
+    //       }
+    //     }
+    //   } catch (error) {
+    //     console.log("error transform", error);
+    //   }
+
+    // },
+  })
 
 
   async function handleLinkedAddressEvent(event: any, contractAddress: string) {
