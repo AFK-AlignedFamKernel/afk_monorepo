b5bc5869349a4b9a706842b98674e7e5c77b4df8
MSGhais <msghais135@gmail.com>
2025-05-13 00:50:17 +0200

    wip

--- apps/pwa/src/app/nostr/create-profile/page.tsx
+++ apps/pwa/src/app/nostr/create-profile/page.tsx
@@ -1,22 +1,10 @@
 'use client';
 
 import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { useNostrContext, settingsStore, authStore, NostrKeyManager } from 'afk_nostr_sdk';
-import { generateRandomKeypair } from '../../../../../../packages/afk_nostr_sdk/src/utils/keypair';
-import * as bip39 from 'bip39';
-import { useUIStore } from '@/store/uiStore';
-import LoginNostrComponent from '@/components/Nostr/login/LoginNostrComponent';
-import { ProfileManagement } from '@/components/Nostr/profile/nostr-profile-management';
 import NostrCreateAccountComponent from '@/components/Nostr/login/NostrCreateAccount';
 export default function NostrCreateProfile() {
-    const [error, setError] = useState('');
-
-    const [type, setType] = useState<'login' | 'register'>('login');
-
     return (
         <div className="">
-
             <NostrCreateAccountComponent />
         </div>
     );
