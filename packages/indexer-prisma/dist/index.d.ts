16496149c6bde0ec610118c45317b5530844cf53
MSGhais <msghais135@gmail.com>
2025-05-14 15:12:41 +0200

    wip turbo

---
+++ packages/indexer-prisma/dist/index.d.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from '../prisma/.prisma/client';
+declare const prisma: PrismaClient<import("../prisma/.prisma/client").Prisma.PrismaClientOptions, never, import("../prisma/.prisma/client/runtime/library").DefaultArgs>;
+export default prisma;
+export { PrismaClient };
+//# sourceMappingURL=index.d.ts.map
---
+++ packages/indexer-prisma/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PrismaClient = void 0;
+const client_1 = require("../prisma/.prisma/client");
+Object.defineProperty(exports, "PrismaClient", { enumerable: true, get: function () { return client_1.PrismaClient; } });
+const prisma = new client_1.PrismaClient();
+exports.default = prisma;
+//# sourceMappingURL=index.js.map
