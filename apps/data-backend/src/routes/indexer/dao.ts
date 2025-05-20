bb13c50731c44a21cd3ac8fe24878593af66dd46
MSGhais <msghais135@gmail.com>
2025-05-21 01:27:15 +0200

    wip fix

--- apps/data-backend/src/routes/indexer/dao.ts
+++ apps/data-backend/src/routes/indexer/dao.ts
@@ -1,57 +1,57 @@
 import type { FastifyInstance, RouteOptions } from 'fastify';
 import { HTTPStatus } from '../../utils/http';
 import { isValidStarknetAddress } from '../../utils/starknet';
-import { eq } from 'drizzle-orm';
 import { db } from 'indexer-v2-db/dist';
 import { daoCreation, daoProposal } from 'indexer-v2-db/dist/schema';
-
+import { eq } from 'indexer-v2-db/node_modules/drizzle-orm';
+// import {eq} from "drizzle-orm"
 interface DaoParams {
   dao_address: string;
 }
 
 async function daoServiceRoute(fastify: FastifyInstance, options: RouteOptions) {
   // Get all daos
-  // fastify.get('/daos', async (request, reply) => {
-  //   try {
-  //     const daos = await db.select().from(daoCreation);
+  fastify.get('/daos', async (request, reply) => {
+    try {
+      const daos = await db.select().from(daoCreation);
 
-  //     reply.status(HTTPStatus.OK).send(daos);
-  //   } catch (error) {
-  //     console.error('Error fetching daos:', error);
-  //     reply.status(HTTPStatus.InternalServerError).send({ message: 'Internal server error.' });
-  //   }
-  // });
+      reply.status(HTTPStatus.OK).send(daos);
+    } catch (error) {
+      console.error('Error fetching daos:', error);
+      reply.status(HTTPStatus.InternalServerError).send({ message: 'Internal server error.' });
+    }
+  });
 
-  // // Get one dao by address
-  // fastify.get<{
-  //   Params: DaoParams;
-  // }>('/daos/:dao_address', async (request, reply) => {
-  //   try {
-  //     const { dao_address } = request.params;
-  //     if (!isValidStarknetAddress(dao_address)) {
-  //       reply.status(HTTPStatus.BadRequest).send({
-  //         code: HTTPStatus.BadRequest,
-  //         message: 'Invalid dao address',
-  //       });
-  //       return;
-  //     }
+  // Get one dao by address
+  fastify.get<{
+    Params: DaoParams;
+  }>('/daos/:dao_address', async (request, reply) => {
+    try {
+      const { dao_address } = request.params;
+      if (!isValidStarknetAddress(dao_address)) {
+        reply.status(HTTPStatus.BadRequest).send({
+          code: HTTPStatus.BadRequest,
+          message: 'Invalid dao address',
+        });
+        return;
+      }
 
-  //     const dao = await db
-  //       .select()
-  //       .from(daoCreation)
-  //       .where(eq(daoCreation.contractAddress, dao_address))
-  //       .limit(1);
+      const dao = await db
+        .select()
+        .from(daoCreation)
+        .where(eq(daoCreation.contractAddress, dao_address))
+        .limit(1);
 
-  //     if (dao.length > 0) {
-  //       reply.status(HTTPStatus.OK).send(dao[0]);
-  //     } else {
-  //       reply.status(HTTPStatus.NotFound).send();
-  //     }
-  //   } catch (error) {
-  //     console.error('Error fetching dao:', error);
-  //     reply.status(HTTPStatus.InternalServerError).send({ message: 'Internal server error.' });
-  //   }
-  // });
+      if (dao.length > 0) {
+        reply.status(HTTPStatus.OK).send(dao[0]);
+      } else {
+        reply.status(HTTPStatus.NotFound).send();
+      }
+    } catch (error) {
+      console.error('Error fetching dao:', error);
+      reply.status(HTTPStatus.InternalServerError).send({ message: 'Internal server error.' });
+    }
+  });
 
   // // Get all proposals for a dao
   // fastify.get<{
--- apps/data-backend/src/routes/indexer/infofi/score-factory.routes.ts
+++ apps/data-backend/src/routes/indexer/infofi/score-factory.routes.ts
@@ -1,7 +1,7 @@
 import type { FastifyInstance, RouteOptions } from 'fastify';
 import { HTTPStatus } from '../../../utils/http';
 import { isValidStarknetAddress } from '../../../utils/starknet';
-import { eq , and, or} from 'drizzle-orm';
+import { eq, and, or } from 'indexer-v2-db/node_modules/drizzle-orm';
 import { db } from 'indexer-v2-db/dist';
 import { contractState, epochState, userEpochState } from 'indexer-v2-db/dist/schema';
 interface ScoreFactoryParams {
@@ -15,104 +15,104 @@ interface ScoreFactoryParamsPerEpoch {
 
 async function subScoreFactoryServiceRoute(fastify: FastifyInstance, options: RouteOptions) {
   // Get all subs
-  // fastify.get('/score-factory/sub', async (request, reply) => {
-  //   try {
-  //     const subs = await db.select().from(contractState);
+  fastify.get('/score-factory/sub', async (request, reply) => {
+    try {
+      const subs = await db.select().from(contractState);
 
-  //     reply.status(HTTPStatus.OK).send(subs);
-  //   } catch (error) {
-  //     console.error('Error fetching subs:', error);
-  //     reply.status(HTTPStatus.InternalServerError).send({ message: 'Internal server error.' });
-  //   }
-  // });
+      reply.status(HTTPStatus.OK).send(subs);
+    } catch (error) {
+      console.error('Error fetching subs:', error);
+      reply.status(HTTPStatus.InternalServerError).send({ message: 'Internal server error.' });
+    }
+  });
 
-  // // Get one sub by address
-  // fastify.get<{
-  //   Params: ScoreFactoryParams;
-  // }>('/score-factory/sub/:sub_address', async (request, reply) => {
-  //   try {
-  //     const { sub_address } = request.params;
-  //     if (!isValidStarknetAddress(sub_address)) {
-  //       reply.status(HTTPStatus.BadRequest).send({
-  //         code: HTTPStatus.BadRequest,
-  //         message: 'Invalid sub address',
-  //       });
-  //       return;
-  //     }
+  // Get one sub by address
+  fastify.get<{
+    Params: ScoreFactoryParams;
+  }>('/score-factory/sub/:sub_address', async (request, reply) => {
+    try {
+      const { sub_address } = request.params;
+      if (!isValidStarknetAddress(sub_address)) {
+        reply.status(HTTPStatus.BadRequest).send({
+          code: HTTPStatus.BadRequest,
+          message: 'Invalid sub address',
+        });
+        return;
+      }
 
-  //     // // Get sub and epoch states with aggregation query
+      // // Get sub and epoch states with aggregation query
 
-  //     const sub = await db
-  //       .select()
-  //       .from(contractState)
-  //       .where(eq(contractState.contract_address, sub_address))
-  //       .limit(1);
+      const sub = await db
+        .select()
+        .from(contractState)
+        .where(eq(contractState.contract_address, sub_address))
+        .limit(1);
 
-  //     console.log('sub', sub);
+      console.log('sub', sub);
 
-  //     const result = await db.query.contractState.findMany({
-  //       where: eq(contractState.contract_address, sub_address),
-  //       with: {
-  //         epochs: true,
-  //         // userProfiles: true,
-  //       },
-  //     });
+      const result = await db.query.contractState.findMany({
+        where: eq(contractState.contract_address, sub_address),
+        with: {
+          epochs: true,
+          // userProfiles: true,
+        },
+      });
 
-  //     console.log('result', result);
-  //     // // Get epoch states for this sub
+      console.log('result', result);
+      // // Get epoch states for this sub
 
-  //     // const epochStates = await db
-  //     //   .select({
-  //     //     epoch_index: epochState.epoch_index,
-  //     //     contract_address: epochState.contract_address,
-  //     //     total_ai_score: epochState.total_ai_score,
-  //     //     total_vote_score: epochState.total_vote_score,
-  //     //     total_amount_deposit: epochState.total_amount_deposit,
-  //     //     total_tip: epochState.total_tip,
-  //     //     amount_claimed: epochState.amount_claimed,
-  //     //     amount_vote: epochState.amount_vote,
-  //     //     amount_algo: epochState.amount_algo,
-  //     //     epoch_duration: epochState.epoch_duration,
-  //     //     start_time: epochState.start_time,
-  //     //     end_time: epochState.end_time,
-  //     //   })
-  //     //   .from(epochState)
-  //     //   .where(eq(epochState.contract_address, sub_address))
-  //     //   .orderBy(epochState.epoch_index);
+      // const epochStates = await db
+      //   .select({
+      //     epoch_index: epochState.epoch_index,
+      //     contract_address: epochState.contract_address,
+      //     total_ai_score: epochState.total_ai_score,
+      //     total_vote_score: epochState.total_vote_score,
+      //     total_amount_deposit: epochState.total_amount_deposit,
+      //     total_tip: epochState.total_tip,
+      //     amount_claimed: epochState.amount_claimed,
+      //     amount_vote: epochState.amount_vote,
+      //     amount_algo: epochState.amount_algo,
+      //     epoch_duration: epochState.epoch_duration,
+      //     start_time: epochState.start_time,
+      //     end_time: epochState.end_time,
+      //   })
+      //   .from(epochState)
+      //   .where(eq(epochState.contract_address, sub_address))
+      //   .orderBy(epochState.epoch_index);
 
-  //     // console.log('epochStates', epochStates);
+      // console.log('epochStates', epochStates);
 
-  //     // const userEpochStates = await db
-  //     //   .select({
-  //     //     nostr_id: userEpochState.nostr_id,
-  //     //     epoch_index: userEpochState.epoch_index,
-  //     //     contract_address: userEpochState.contract_address,
-  //     //     total_tip: userEpochState.total_tip,
-  //     //     total_ai_score: userEpochState.total_ai_score,
-  //     //     total_vote_score: userEpochState.total_vote_score,
-  //     //     amount_claimed: userEpochState.amount_claimed,
-  //     //     created_at: userEpochState.created_at,
-  //     //     updated_at: userEpochState.updated_at,
-  //     //   })
-  //     //   .from(userEpochState)
-  //     //   .where(eq(userEpochState.contract_address, sub_address))
-  //     //   .groupBy(userEpochState.epoch_index);
+      // const userEpochStates = await db
+      //   .select({
+      //     nostr_id: userEpochState.nostr_id,
+      //     epoch_index: userEpochState.epoch_index,
+      //     contract_address: userEpochState.contract_address,
+      //     total_tip: userEpochState.total_tip,
+      //     total_ai_score: userEpochState.total_ai_score,
+      //     total_vote_score: userEpochState.total_vote_score,
+      //     amount_claimed: userEpochState.amount_claimed,
+      //     created_at: userEpochState.created_at,
+      //     updated_at: userEpochState.updated_at,
+      //   })
+      //   .from(userEpochState)
+      //   .where(eq(userEpochState.contract_address, sub_address))
+      //   .groupBy(userEpochState.epoch_index);
 
   
-  //     if (sub.length > 0) {
-  //       reply.status(HTTPStatus.OK).send({
-  //         sub:sub[0],
-  //         // epochs: epochStates
-  //         epochs: result?.[0]?.epochs
-  //       });
-  //     } else {
-  //       reply.status(HTTPStatus.NotFound).send();
-  //     }
-  //   } catch (error) {
-  //     console.error('Error fetching sub:', error);
-  //     reply.status(HTTPStatus.InternalServerError).send({ message: 'Internal server error.' });
-  //   }
-  // });
+      if (sub.length > 0) {
+        reply.status(HTTPStatus.OK).send({
+          sub:sub[0],
+          // epochs: epochStates
+          epochs: result?.[0]?.epochs
+        });
+      } else {
+        reply.status(HTTPStatus.NotFound).send();
+      }
+    } catch (error) {
+      console.error('Error fetching sub:', error);
+      reply.status(HTTPStatus.InternalServerError).send({ message: 'Internal server error.' });
+    }
+  });
 
   //   // Get one sub by address
   //   fastify.get<{
