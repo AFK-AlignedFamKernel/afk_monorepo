1ffb3b6fac2fa4c633050b4cd316c4eb589644ff
MSGhais <msghais135@gmail.com>
2025-05-21 17:16:03 +0200

    wip

---
+++ apps/pwa/src/app/discovery/page.tsx
@@ -0,0 +1,10 @@
+import DiscoveryComponent from "@/components/Discovery";
+
+export default function Discovery() {
+  return (
+    <div>
+      <h1>Discovery</h1>
+      <DiscoveryComponent></DiscoveryComponent>
+    </div>
+  );
+}
