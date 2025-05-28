676133d9c0af55f7418a9b3691e3703d4d984b6c
MSGhais <msghais135@gmail.com>
2025-05-28 14:59:33 +0200

    fix build

--- apps/pwa/next.config.mjs
+++ apps/pwa/next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     
