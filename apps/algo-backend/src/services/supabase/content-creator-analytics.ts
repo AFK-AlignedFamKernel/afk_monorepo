69cfa09000ba4ad681ab78d7b7bba370824e7006
MSGhais <msghais135@gmail.com>
2025-06-08 00:51:18 +0200

    algo + model update

--- apps/algo-backend/src/services/supabase/content-creator-analytics.ts
+++ apps/algo-backend/src/services/supabase/content-creator-analytics.ts
@@ -52,6 +52,15 @@ export class ContentCreatorAnalyticsService {
             console.log("creatorsIdentities", creatorsIdentities);
             console.log("socialIdentities", socialIdentities);
 
+
+            let socials_llm_classification: any[] = [];
+            let socials_scores: any[] = [];
+            let reputation_scores: any[] = [];
+            let reputation: any = {};
+            let topics: string[] = [];
+
+            let twitterAnalytics: any;
+
             if (creatorsIdentities && creatorsIdentities.length > 0) {
 
                 for (let x of creatorsIdentities) {
@@ -64,7 +73,7 @@ export class ContentCreatorAnalyticsService {
                         if (user_name) {
                             console.log("user_name twitter found", user_name);
                             let resultTwitterAnalytics: any;
-                            let twitterAnalytics = await this.contentCreatorAnalytics.getTwitterAnalytics(user_name);
+                            twitterAnalytics = await this.contentCreatorAnalytics.getTwitterAnalytics(user_name);
                             console.log("twitterAnalytics processData", twitterAnalytics?.processData);
                             console.log("twitterAnalytics llm output", twitterAnalytics?.result);
 
@@ -75,37 +84,16 @@ export class ContentCreatorAnalyticsService {
                                 console.log("twitterAnalytics llm output", twitterAnalytics?.result);
                             }
 
-                            const { data: dataCreator, error: errorCreator } = await supabase.from('creator_analytics').select('*').eq('creator_id', creator.id).eq('platform', 'twitter');
-                            console.log("dataCreator exist", true);
-                            console.log("errorCreator", errorCreator);
-                            if (dataCreator && dataCreator.length > 0 && errorCreator === null) {
-                                console.log("dataCreator", dataCreator);
-                                console.log("errorCreator", errorCreator);
-                                const { data, error } = await supabase.from('creator_analytics').update({
-                                    llm_classification: twitterAnalytics?.result ?? dataCreator[0].llm_classification,
-                                    llm_process_data: twitterAnalytics?.processData ?? dataCreator[0].llm_process_data,
-                                    recommendations: twitterAnalytics?.recommendations ?? dataCreator[0].recommendations,
-                                    stats_creator: twitterAnalytics?.stats_creator ?? dataCreator[0].stats_creator,
-                                    stats_content: twitterAnalytics?.stats_content ?? dataCreator[0].stats_content,
-                                }).eq('id', dataCreator[0].id).eq('creator_id', creator.id).eq('platform', 'twitter').select().single();
-                                console.log("data", data);
-                                console.log("error", error);
-                            } else {
-                                console.log("dataCreator", dataCreator);
-                                console.log("errorCreator", errorCreator);
-                                const { data, error } = await supabase.from('creator_analytics').upsert({
-                                    creator_id: creator.id,
-                                    platform: 'twitter',
-                                    llm_classification: twitterAnalytics?.result,
-                                    llm_process_data: twitterAnalytics?.processData,
-                                    recommendations: twitterAnalytics?.recommendations,
-                                    stats_creator: twitterAnalytics?.stats_creator,
-                                    stats_content: twitterAnalytics?.stats_content,
-                                }).select().single();
-                            }
+
+                            socials_llm_classification.push({
+                                platform: 'twitter',
+                                classification: twitterAnalytics?.result,
+                                updated_at: new Date()
+                            });
+                            topics.push(twitterAnalytics?.result?.topics);
+
+                      
                         }
-
-
 
                     }
 
@@ -116,6 +104,37 @@ export class ContentCreatorAnalyticsService {
                     if (x.identity_data.provider === 'youtube') {
                         console.log("identity on youtube", x);
                     }
+                }
+
+                const { data: dataCreator, error: errorCreator } = await supabase.from('creator_analytics').select('*').eq('creator_id', creator.id).eq('platform', 'twitter');
+                console.log("dataCreator exist", true);
+                console.log("errorCreator", errorCreator);
+                if (dataCreator && dataCreator.length > 0 && errorCreator === null) {
+                    console.log("dataCreator", dataCreator);
+                    console.log("errorCreator", errorCreator);
+                    const { data, error } = await supabase.from('creator_analytics').update({
+                        llm_classification: twitterAnalytics?.result ?? dataCreator[0].llm_classification,
+                        llm_process_data: twitterAnalytics?.processData ?? dataCreator[0].llm_process_data,
+                        recommendations: twitterAnalytics?.recommendations ?? dataCreator[0].recommendations,
+                        stats_creator: twitterAnalytics?.stats_creator ?? dataCreator[0].stats_creator,
+                        stats_content: twitterAnalytics?.stats_content ?? dataCreator[0].stats_content,
+                        socials_llm_classification: socials_llm_classification ?? dataCreator[0].socials_llm_classification,
+                    }).eq('id', dataCreator[0].id).eq('creator_id', creator.id).eq('platform', 'twitter').select().single();
+                    console.log("data", data);
+                    console.log("error", error);
+                } else {
+                    console.log("dataCreator", dataCreator);
+                    console.log("errorCreator", errorCreator);
+                    const { data, error } = await supabase.from('creator_analytics').upsert({
+                        creator_id: creator.id,
+                        platform: 'twitter',
+                        llm_classification: twitterAnalytics?.result,
+                        llm_process_data: twitterAnalytics?.processData,
+                        recommendations: twitterAnalytics?.recommendations,
+                        stats_creator: twitterAnalytics?.stats_creator,
+                        stats_content: twitterAnalytics?.stats_content,
+                        socials_llm_classification: socials_llm_classification,
+                    }).select().single();
                 }
             }
 
