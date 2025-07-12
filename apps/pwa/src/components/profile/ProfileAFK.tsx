61193d4730c03b204e9e312641ea3e01567f758c
MSGhais <msghais135@gmail.com>
2025-07-12 15:49:18 +0200

    comment nip1 msg in profile

--- apps/pwa/src/components/profile/ProfileAFK.tsx
+++ apps/pwa/src/components/profile/ProfileAFK.tsx
@@ -71,7 +71,7 @@ export default function ProfileAfk() {
                 </svg>
               </button>
 
-              <button onClick={() => handleTabChange("messages")} className="btn w-full flex justify-between border-2 border-contrast-200 rounded-lg">
+              {/* <button onClick={() => handleTabChange("messages")} className="btn w-full flex justify-between border-2 border-contrast-200 rounded-lg">
                 <div>
                   <p className="font-semibold">Messages</p>
                   <p className="text-sm opacity-80">Manage your messages</p>
@@ -79,7 +79,7 @@ export default function ProfileAfk() {
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
                 </svg>
-              </button>
+              </button> */}
 
               {/* <Link href="/brand" className="btn btn-gradient-blue w-full flex items-center justify-between">
               <div>
