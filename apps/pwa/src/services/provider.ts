75b2dfc3ece1fec821ca65f8e2cc95854d05ff14
MSGhais <msghais135@gmail.com>
2025-07-15 17:05:06 +0200

    undo throw error

--- apps/pwa/src/services/provider.ts
+++ apps/pwa/src/services/provider.ts
@@ -2,7 +2,7 @@ import {constants, RpcProvider} from 'starknet';
 
 import {NETWORK_NAME} from '@/constants/misc';
 
-if (!process.env.PROVIDER_URL) throw new Error('PROVIDER_URL is not set');
+// if (!process.env.PROVIDER_URL) throw new Error('PROVIDER_URL is not set');
 
 export const provider = new RpcProvider({
   nodeUrl: process.env.PROVIDER_URL,
