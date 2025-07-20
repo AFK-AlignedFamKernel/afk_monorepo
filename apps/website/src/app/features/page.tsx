85a43fe3ece62ed0dc48b253cb025961d788a6eb
MSGhais <msghais135@gmail.com>
2025-07-20 04:45:03 +0200

    fix build website

--- apps/website/src/app/features/page.tsx
+++ apps/website/src/app/features/page.tsx
@@ -1,13 +1,13 @@
 import {Footer} from '../../components/Footer';
-import {ContributeSection} from '../../components/landing/ContributeSection';
-import {DescriptionFeatures} from '../../components/landing/features/DescriptionFeatures';
+import {ContributeSection} from '../../components/all_in_one/ContributeSection';
+import {DescriptionSection} from '../../components/all_in_one/DescriptionSection';
 import {Navbar} from '../../components/Navbar';
 
 export default function Features() {
   return (
     <div className="min-h-screen w-full relative bg-black">
       <Navbar />
-      <DescriptionFeatures />
+      <DescriptionSection />
       <div className="flex flex-col desktop:gap-y-[80px] gap-y-[50px] mt-[50px] desktop:mt-[180px]">
         <ContributeSection />
       </div>
