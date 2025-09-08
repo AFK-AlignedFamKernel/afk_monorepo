6977e6a825e324165fadb51de4d1d623d2ebdf75
MSGhais <msghais135@gmail.com>
2025-09-08 14:34:56 +0200

    clean

--- packages/indexer-v2-db/.drizzle/0002_stale_maverick.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "candlesticks" ALTER COLUMN "open" SET DATA TYPE numeric;--> statement-breakpoint
-ALTER TABLE "candlesticks" ALTER COLUMN "high" SET DATA TYPE numeric;--> statement-breakpoint
-ALTER TABLE "candlesticks" ALTER COLUMN "low" SET DATA TYPE numeric;--> statement-breakpoint
-ALTER TABLE "candlesticks" ALTER COLUMN "close" SET DATA TYPE numeric;
